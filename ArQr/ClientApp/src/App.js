d7e32d225e22f7e2aeb5136666beddcf18dca61c
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-06-07 09:58:17 +0430

    Move the custom style file into the styles folder and update it's reference.

--- ArQr/ClientApp/src/App.js
+++ ArQr/ClientApp/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route} from 'react-router';
 
 import Home from './components/Home';
-import './custom.css'
+import './styles/custom.css'
 
 const App = () => {
     return (
