e085e732c06b1b469e61247249dfd8db6d116fd7
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-06-09 03:05:39 +0430

    Modify the home component import statement in the app component.

--- ArQr/ClientApp/src/App.js
+++ ArQr/ClientApp/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Route} from 'react-router';
 
-import Home from './components/Home';
+import Home from './components/HomePage';
 
 const App = () => {
     return (
