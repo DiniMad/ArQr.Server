cb11b93155ff871ad9dcda605b6b242616538cb3
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-06-09 09:36:29 +0430

    Add onFormError function to form props to set errors and remove the initial value form props and create it in the component.

--- ArQr/ClientApp/src/components/HomePage/Form.js
+++ ArQr/ClientApp/src/components/HomePage/Form.js
@@ -3,7 +3,21 @@ import PropTypes from 'prop-types';
 import {Formik, Form as FormikForm, Field} from 'formik';
 import * as Yup from 'yup';
 
-const Form = ({onChangeFormButtonClick, initialValues, onSubmit, registerMode}) => {
+const Form = ({onSubmit, onFormError, onChangeFormButtonClick, registerMode}) => {
+    const initialValues = {
+        email: '',
+        password: ''
+    };
+
+    let passwordConfirmation = null;
+    if (registerMode) {
+        initialValues.passwordConfirmation = '';
+        passwordConfirmation = Yup.string()
+                                  .required('تکرار پسورد وارد نشده است.')
+                                  .oneOf([Yup.ref('password')], 'تکرار پسورد با پسورد مطابقت ندارد.');
+    }
+
+
     const validationSchema = Yup.object(
         {
             email: Yup.string().email('ایمیل صحیح نمی باشد.')
@@ -11,46 +25,44 @@ const Form = ({onChangeFormButtonClick, initialValues, onSubmit, registerMode}) 
             password: Yup.string()
                          .required('پسورد وارد نشده است.')
                          .min(4, 'پسورد نمی تواند کمتر از 4 کارکتر باشد.')
-                         .matches(/^[a-zA-Z0-9!@#$&*]*$/, 'کارکتر وارد شده مجاز نمی باشد.'),
-            passwordConfirmation: Yup.string()
-                                     .required('تکرار پسورد وارد نشده است.')
-                                     .oneOf([Yup.ref('password')], 'تکرار پسورد با پسورد مطابقت ندارد.')
+                         .matches(/^[a-zA-Z0-9!@#$&*]*$/, 'کارکتر وارد شده در پسورد مجاز نمی باشد.'),
+            passwordConfirmation: Yup.string().concat(passwordConfirmation),
         }
     );
 
+
     return (
         <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit} regi>
-            {({errors}) =>
-                <div id='form'>
+            {({errors}) => {
+                onFormError(errors);
+
+                return (<div id='form'>
                     <FormikForm className='form'>
-                        <div className='input'>
-                            <Field id='email' name='email' type='email'/>
-                            <label htmlFor='email'>{errors.email}</label>
+                        <div className='inputs'>
+                            <Field id='email' name='email' type='email' placeholder='ایمیل'/>
+                            <Field id='password' name='password' type='password' placeholder='رمز عبور'/>
+                            {registerMode &&
+                            <Field id='passwordConfirmation'
+                                   name='passwordConfirmation'
+                                   type='password'
+                                   placeholder='تکرار رمز عبور'/>
+                            }
                         </div>
-                        <div className='input'>
-                            <Field id='password' name='password' type='password'/>
-                            <label htmlFor='password'>{errors.password}</label>
-                        </div>
-                        {registerMode &&
-                        <div className='input'>
-                            <Field id='passwordConfirmation' name='passwordConfirmation' type='password'/>
-                            <label htmlFor='passwordConfirmation'>{errors.passwordConfirmation}</label>
-                        </div>
-                        }
                         <Field name='submit' type='submit' value={registerMode ? 'ایجاد حساب' : 'ورود'}/>
                     </FormikForm>
                     <button onClick={onChangeFormButtonClick} type='button'>
                         {registerMode ? 'ورود به حساب کاربری' : 'ایجاد حساب کاربری'}
                     </button>
-                </div>
+                </div>);
+            }
             }
         </Formik>
     );
 };
 
 Form.propTypes = {
-    initialValues: PropTypes.object.isRequired,
     onSubmit: PropTypes.func.isRequired,
+    onFormError: PropTypes.func.isRequired,
     onChangeFormButtonClick: PropTypes.func.isRequired,
     registerMode: PropTypes.bool
 };
