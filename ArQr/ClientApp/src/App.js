160723635071186275d933048824d0ecc357f36b
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-06-11 11:22:54 +0430

    Modify the app component to import regular or mobile version of style file based on the browser width.

--- ArQr/ClientApp/src/App.js
+++ ArQr/ClientApp/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {Route} from 'react-router';
-
 import Home from './components/HomePage';
-import './styles/style.css'
+
+if (window.innerWidth >= 760)
+    require('./styles/style.css');
+else
+    require('./styles/style.mobile.css');
 
 const App = () => {
     return (
