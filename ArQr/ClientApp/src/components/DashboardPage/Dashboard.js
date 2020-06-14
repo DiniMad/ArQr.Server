2cd1f931c81b9c18cd78f85e1da4b6d3a50c5017
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-06-14 17:34:00 +0430

    Implement the dashboard component with test data.

--- ArQr/ClientApp/src/components/DashboardPage/Dashboard.js
+++ ArQr/ClientApp/src/components/DashboardPage/Dashboard.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
+import ProductItem from './ProductItem';
+
+const TEST_PRODUCT_NAME = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, quos!';
+const TEST_QR_VALUE = 'http://facebook.github.io/react/';
+
 const Dashboard = () => {
     return (
         <div id="dashboard">
-            Dashboard
+            <div id="products">
+                <ProductItem link='#' qrValue='' productName='افزودن'/>
+                <ProductItem link='#' qrValue={TEST_QR_VALUE} productName={TEST_PRODUCT_NAME}/>
+                <ProductItem link='#' qrValue={TEST_QR_VALUE} productName={TEST_PRODUCT_NAME}/>
+                <ProductItem link='#' qrValue={TEST_QR_VALUE} productName={TEST_PRODUCT_NAME}/>
+                <ProductItem link='#' qrValue={TEST_QR_VALUE} productName={TEST_PRODUCT_NAME}/>
+                <ProductItem link='#' qrValue={TEST_QR_VALUE} productName={TEST_PRODUCT_NAME}/>
+                <ProductItem link='#' qrValue={TEST_QR_VALUE} productName={TEST_PRODUCT_NAME}/>
+                <ProductItem link='#' qrValue={TEST_QR_VALUE} productName={TEST_PRODUCT_NAME}/>
+                <ProductItem link='#' qrValue={TEST_QR_VALUE} productName={TEST_PRODUCT_NAME}/>
+                <ProductItem link='#' qrValue={TEST_QR_VALUE} productName={TEST_PRODUCT_NAME}/>
+                <ProductItem link='#' qrValue={TEST_QR_VALUE} productName={TEST_PRODUCT_NAME}/>
+                <ProductItem link='#' qrValue={TEST_QR_VALUE} productName={TEST_PRODUCT_NAME}/>
+            </div>
         </div>
     );
 };
