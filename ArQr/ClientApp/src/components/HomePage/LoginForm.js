b5bcf04f2bb053be7928a5de5413055fa1acf977
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-06-22 13:39:43 +0430

    Modify the login form component to navigate user to dashboard on login button clicked.

--- ArQr/ClientApp/src/components/HomePage/LoginForm.js
+++ ArQr/ClientApp/src/components/HomePage/LoginForm.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {useHistory} from 'react-router-dom';
+
 
 import Form from './Form';
 
 const LoginForm = ({onChangeFormButtonClick, onFormError}) => {
+    const history = useHistory();
+
     const onSubmit = (data) => {
         // TODO: Send Request to the server
         console.log(data);
+        history.replace('/dashboard');
     };
 
     return (
