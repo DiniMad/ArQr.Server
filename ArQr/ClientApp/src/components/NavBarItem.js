6693c6405d40160c983c4f29e8e342bba3c92f29
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-11 19:27:15 +0430

    Modify the NavBarItem component to return button to Link based on the onClick parameter.

--- ArQr/ClientApp/src/components/NavBarItem.js
+++ ArQr/ClientApp/src/components/NavBarItem.js
@@ -3,20 +3,26 @@ import PropTypes from 'prop-types';
 import {Link, useLocation} from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
-const NavBarItem = ({linkAddress, text, icon, iconSize}) => {
+const NavBarItem = ({linkAddress, onClick, text, icon, iconSize}) => {
     const location = useLocation();
     const navClasses = location.pathname === linkAddress ? 'nav-item selected' : 'nav-item';
 
-    return (
-        <Link to={linkAddress} className={navClasses}>
-            <div className="text">
-                <p>{text}</p>
-            </div>
-            <div className="icon">
-                <FontAwesomeIcon icon={icon} size={iconSize}/>
-            </div>
-        </Link>
-    );
+    return onClick ?
+           <button onClick={onClick} className={navClasses}>{content()}</button> :
+           <Link to={linkAddress} className={navClasses}>{content()}</Link>;
+
+    function content() {
+        return (
+            <>
+                <div className="text">
+                    <p>{text}</p>
+                </div>
+                <div className="icon">
+                    <FontAwesomeIcon icon={icon} size={iconSize}/>
+                </div>
+            </>
+        );
+    }
 };
 
 NavBarItem.propTypes =
@@ -24,7 +30,8 @@ NavBarItem.propTypes =
         linkAddress: PropTypes.string.isRequired,
         text: PropTypes.string.isRequired,
         icon: PropTypes.object.isRequired,
-        iconSize: PropTypes.string.isRequired
+        iconSize: PropTypes.string.isRequired,
+        onClick: PropTypes.func
     };
 
 export default NavBarItem;
