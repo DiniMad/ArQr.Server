627e42e64cf7b648b3bd76c71984beb7221b2718
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-11 16:55:20 +0430

    Modify the AuthorizeRoute to only keep the return path.

--- ArQr/ClientApp/src/components/AuthorizeRoute.js
+++ ArQr/ClientApp/src/components/AuthorizeRoute.js
@@ -2,13 +2,13 @@ import React, {useContext} from 'react';
 import {Redirect, Route} from 'react-router-dom';
 
 import {AccessToken} from './contexts/AccessToken';
-import {urls} from './services/constants';
+import {queryParameters, urls} from './services/constants';
 
 const AuthorizeRoute = ({component: Component, ...rest}) => {
     const link = document.createElement('a');
     link.href = rest.path;
-    const returnUrl = `${link.protocol}//${link.host}${link.pathname}${link.search}${link.hash}`;
-    const redirectUrl = `${urls.homePage}?returnUrl=${encodeURI(returnUrl)}`;
+    const returnPath = `${link.pathname}${link.search}${link.hash}`;
+    const redirectUrl = `${urls.homePage}?${queryParameters.returnPath}=${encodeURI(returnPath)}`;
 
     const [accessToken] = useContext(AccessToken);
     return <Route {...rest}
