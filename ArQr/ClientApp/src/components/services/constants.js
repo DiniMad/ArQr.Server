1990e754769e4bf8d251c43cd3adcb8f2df1c584
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-11 15:24:19 +0430

    Create file to store all the constants.

---
+++ ArQr/ClientApp/src/components/services/constants.js
@@ -0,0 +1,14 @@
+export const urls = {
+    login: 'connect/token'
+};
+
+export const oidc = {
+    client_id: {key: 'client_id', value: 'ArQr'},
+    grant_type: {key: 'grant_type', value: 'password'},
+    scope: {key: 'scope', value: 'ArQrAPI offline_access'},
+    refresh_token: {key: 'refresh_token'}
+};
+
+export const httpStatusCode = {
+    success: 200
+};
