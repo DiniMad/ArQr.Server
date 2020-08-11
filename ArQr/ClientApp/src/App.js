8e6163cc5da3f4c680c99b0f82cc3d142a9de0b7
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-11 15:23:41 +0430

    Wrap-up the routes in the App component with the AcessTokenProvider component.

--- ArQr/ClientApp/src/App.js
+++ ArQr/ClientApp/src/App.js
@@ -7,6 +7,7 @@ import Dashboard from './components/DashboardPage';
 import Product from './components/ProductPage';
 import NotFound from './components/NotFoundPage';
 import withLayout from './components/higherOrderComponents/withLayout';
+import AccessTokenProvider from './components/contexts/AccessToken';
 
 if (window.innerWidth >= 760)
     require('./styles/style.css');
@@ -16,11 +17,13 @@ else
 const App = () => {
     return (
         <Switch>
-            <Route exact path='/' component={Home}/>
-            {/*TODO: Change the route components below into the authorize route */}
-            <Route path='/dashboard' component={withLayout(Dashboard)}/>
-            <Route path='/product' component={withLayout(Product)}/>
-            <Route path='*' component={NotFound}/>
+            <AccessTokenProvider>
+                <Route exact path='/' component={Home}/>
+                {/*TODO: Change the route components below into the authorize route */}
+                <Route path='/dashboard' component={withLayout(Dashboard)}/>
+                <Route path='/product' component={withLayout(Product)}/>
+                <Route path='*' component={NotFound}/>
+            </AccessTokenProvider>
         </Switch>
     );
 };
