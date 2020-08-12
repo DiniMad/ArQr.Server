5fdd0aad42776a3be2adfe3e5d08c4e265822c32
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-12 15:25:10 +0430

    Update the useLogin hook to store user detail extracted from the access token in the UserContext.

--- ArQr/ClientApp/src/components/hooks/useLogin.js
+++ ArQr/ClientApp/src/components/hooks/useLogin.js
@@ -1,7 +1,10 @@
 import {useContext} from 'react';
+import * as jwtDecode from 'jwt-decode';
+
 import http from '../services/http';
 import {AccessTokenContext} from '../contexts/AccessTokenContext';
 import {httpStatusCode, oidc, urls} from '../services/constants';
+import {UserContext} from '../contexts/UserContext';
 
 const data = new FormData();
 data.append(oidc.client_id.key, oidc.client_id.value);
@@ -10,6 +13,7 @@ data.append(oidc.scope.key, oidc.scope.value);
 
 const useLogin = () => {
     const [, setAccessToken] = useContext(AccessTokenContext);
+    const [, setUser] = useContext(UserContext);
 
     return async (username, password) => {
         data.append('username', username);
@@ -19,6 +23,17 @@ const useLogin = () => {
         if (response.status === httpStatusCode.success) {
             setAccessToken(response.data.access_token);
             localStorage.setItem(oidc.refresh_token.key, response.data.refresh_token);
+            
+            const tokenData = jwtDecode(response.data.access_token);
+            const user = {
+                id: tokenData.sub,
+                username: tokenData.username,
+                email: tokenData.email,
+                emailConfirmed: tokenData.emailConfirmed === 'True',
+                phoneNumber: tokenData.phoneNumber,
+                phoneNumberConfirmed: tokenData.phoneNumberConfirmed === 'True',
+            };
+            setUser(user);
             return true;
         }
         return false;
