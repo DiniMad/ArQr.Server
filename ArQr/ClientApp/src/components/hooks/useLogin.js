42b23e162a0582842f70a121c572d1f1a41992fb
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-12 15:47:41 +0430

    Modify both useLogin and useRefreshToken custom hooks to use the useIdentityStore hook instead of storing data directly.

--- ArQr/ClientApp/src/components/hooks/useLogin.js
+++ ArQr/ClientApp/src/components/hooks/useLogin.js
@@ -1,10 +1,6 @@
-import {useContext} from 'react';
-import * as jwtDecode from 'jwt-decode';
-
+import useIdentityStore from './useIdentityStore';
 import http from '../services/http';
-import {AccessTokenContext} from '../contexts/AccessTokenContext';
 import {httpStatusCode, oidc, urls} from '../services/constants';
-import {UserContext} from '../contexts/UserContext';
 
 const data = new FormData();
 data.append(oidc.client_id.key, oidc.client_id.value);
@@ -12,8 +8,7 @@ data.append(oidc.grant_type.key, oidc.grant_type.password);
 data.append(oidc.scope.key, oidc.scope.value);
 
 const useLogin = () => {
-    const [, setAccessToken] = useContext(AccessTokenContext);
-    const [, setUser] = useContext(UserContext);
+    const storeIdentities=useIdentityStore()
 
     return async (username, password) => {
         data.append('username', username);
@@ -21,19 +16,7 @@ const useLogin = () => {
 
         const response = await http.post(urls.tokenEndPoint, data);
         if (response.status === httpStatusCode.success) {
-            setAccessToken(response.data.access_token);
-            localStorage.setItem(oidc.refresh_token.key, response.data.refresh_token);
-            
-            const tokenData = jwtDecode(response.data.access_token);
-            const user = {
-                id: tokenData.sub,
-                username: tokenData.username,
-                email: tokenData.email,
-                emailConfirmed: tokenData.emailConfirmed === 'True',
-                phoneNumber: tokenData.phoneNumber,
-                phoneNumberConfirmed: tokenData.phoneNumberConfirmed === 'True',
-            };
-            setUser(user);
+            storeIdentities(response.data.access_token,response.data.refresh_token)
             return true;
         }
         return false;
--- ArQr/ClientApp/src/components/hooks/useRefreshToken.js
+++ ArQr/ClientApp/src/components/hooks/useRefreshToken.js
@@ -1,38 +1,22 @@
-import {useContext} from 'react';
-import * as jwtDecode from 'jwt-decode';
-
-import {httpStatusCode, oidc, urls} from '../services/constants';
+import useIdentityStore from './useIdentityStore';
 import http from '../services/http';
-import {AccessTokenContext} from '../contexts/AccessTokenContext';
-import {UserContext} from '../contexts/UserContext';
+import {httpStatusCode, oidc, urls} from '../services/constants';
 
 const data = new FormData();
 data.append(oidc.client_id.key, oidc.client_id.value);
 data.append(oidc.grant_type.key, oidc.grant_type.refreshToken);
 
 const useRefreshToken = () => {
-    const [, setAccessToken] = useContext(AccessTokenContext);
-    const [, setUser] = useContext(UserContext);
+    const storeIdentities=useIdentityStore();
 
     return async () => {
         const refreshToken = localStorage.getItem(oidc.refresh_token.key);
         if (!refreshToken) return false;
+        
         data.append(oidc.grant_type.refreshToken, refreshToken);
         const response = await http.post(urls.tokenEndPoint, data);
         if (response.status === httpStatusCode.success) {
-            setAccessToken(response.data.access_token);
-            localStorage.setItem(oidc.refresh_token.key, response.data.refresh_token);
-            
-            const tokenData = jwtDecode(response.data.access_token);
-            const user = {
-                id: tokenData.sub,
-                username: tokenData.username,
-                email: tokenData.email,
-                emailConfirmed: tokenData.emailConfirmed === 'True',
-                phoneNumber: tokenData.phoneNumber,
-                phoneNumberConfirmed: tokenData.phoneNumberConfirmed === 'True',
-            };
-            setUser(user);
+            storeIdentities(response.data.access_token,response.data.refresh_token)
             return true;
         }
         return false;
