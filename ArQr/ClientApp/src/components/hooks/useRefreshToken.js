d8b20638c2f1de9bd84f6d097714b8922cde8259
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-12 15:25:43 +0430

    Update the useRefreshToken hook to store user detail extracted from the access token in the UserContext.

--- ArQr/ClientApp/src/components/hooks/useRefreshToken.js
+++ ArQr/ClientApp/src/components/hooks/useRefreshToken.js
@@ -1,7 +1,10 @@
 import {useContext} from 'react';
+import * as jwtDecode from 'jwt-decode';
+
 import {httpStatusCode, oidc, urls} from '../services/constants';
 import http from '../services/http';
 import {AccessTokenContext} from '../contexts/AccessTokenContext';
+import {UserContext} from '../contexts/UserContext';
 
 const data = new FormData();
 data.append(oidc.client_id.key, oidc.client_id.value);
@@ -9,6 +12,7 @@ data.append(oidc.grant_type.key, oidc.grant_type.refreshToken);
 
 const useRefreshToken = () => {
     const [, setAccessToken] = useContext(AccessTokenContext);
+    const [, setUser] = useContext(UserContext);
 
     return async () => {
         const refreshToken = localStorage.getItem(oidc.refresh_token.key);
@@ -18,6 +22,17 @@ const useRefreshToken = () => {
         if (response.status === httpStatusCode.success) {
             setAccessToken(response.data.access_token);
             localStorage.setItem(oidc.refresh_token.key, response.data.refresh_token);
+            
+            const tokenData = jwtDecode(response.data.access_token);
+            const user = {
+                id: tokenData.sub,
+                username: tokenData.username,
+                email: tokenData.email,
+                emailConfirmed: tokenData.emailConfirmed === 'True',
+                phoneNumber: tokenData.phoneNumber,
+                phoneNumberConfirmed: tokenData.phoneNumberConfirmed === 'True',
+            };
+            setUser(user);
             return true;
         }
         return false;
