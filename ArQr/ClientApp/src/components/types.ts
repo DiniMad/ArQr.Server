51818a189c0aa0ea7be65d56df0280cbceffebdf
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-15 23:20:03 +0430

    Add the UserType to the types file.

--- ArQr/ClientApp/src/components/types.ts
+++ ArQr/ClientApp/src/components/types.ts
@@ -4,3 +4,12 @@ export type CreateContextForUseState<T> = [
     T | null,
     (value: T | null) => void | ((value: T) => T)
 ] | null;
+
+export type UserType = {
+    id: string
+    username: string
+    email: string
+    emailConfirmed: boolean
+    phoneNumber: string
+    phoneNumberConfirmed: boolean
+}
