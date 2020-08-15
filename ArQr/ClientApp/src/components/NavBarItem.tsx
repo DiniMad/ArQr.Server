5806478629b99794ec2663499d1a44765e9420e1
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-15 18:49:28 +0430

    Migrate the NavBarItem component to typescript.

---
+++ ArQr/ClientApp/src/components/NavBarItem.tsx
@@ -0,0 +1,34 @@
+import React, {MouseEventHandler} from "react";
+import {Link, useLocation} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {IconProp, SizeProp} from "@fortawesome/fontawesome-svg-core";
+
+type props = {
+    handleClick: string | MouseEventHandler,
+    text: string,
+    icon: IconProp,
+    iconSize: SizeProp
+}
+const NavBarItem = ({handleClick, text, icon, iconSize}: props) => {
+    const location = useLocation();
+    const navClasses = location.pathname === handleClick ? "nav-item selected" : "nav-item";
+
+    return typeof handleClick === "string" ?
+           <Link to={handleClick} className={navClasses}>{content()}</Link> :
+           <button onClick={handleClick} className={navClasses}>{content()}</button>;
+
+    function content() {
+        return (
+            <>
+                <div className="text">
+                    <p>{text}</p>
+                </div>
+                <div className="icon">
+                    <FontAwesomeIcon icon={icon} size={iconSize}/>
+                </div>
+            </>
+        );
+    }
+};
+
+export default NavBarItem;
