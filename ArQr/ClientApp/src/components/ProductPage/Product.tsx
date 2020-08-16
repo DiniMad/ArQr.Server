365630c0de58343e53846b8d7bdff0ba7e965a05
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-16 15:48:58 +0430

    Migrate the Product component to typescript.

--- ArQr/ClientApp/src/components/ProductPage/Product.tsx
+++ ArQr/ClientApp/src/components/ProductPage/Product.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import {useForm} from 'react-hook-form';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faImage, faVideo} from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import {useForm} from "react-hook-form";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faImage, faVideo} from "@fortawesome/free-solid-svg-icons";
 
-import TextInput from './TextInput';
-import ContentInput from './ContentInput';
-import {Phone} from '../../images';
-import {PICTURE, TEXT, VIDEO} from './constants';
+import TextInput from "./TextInput";
+import ContentInput from "./ContentInput";
+import {Phone} from "../../images";
+import {AdsProduct} from "../types";
 
 const Product = () => {
-    const initialValues = {
-        title: '',
-        description: '',
-        content: {
-            type: TEXT,
-            value: ''
+    const initialValues: AdsProduct = {
+        title      : "",
+        description: "",
+        content    : {
+            type : "Text",
+            value: ""
         }
     };
 
-    const {register, watch, setValue, handleSubmit: handleFormSubmit} = useForm({defaultValues: initialValues});
-    const {title, description, content} = watch(['title', 'description', 'content']);
-
-    const handelSubmit = (data) => {
-        console.log(data);
+    const {register, watch, setValue, handleSubmit: handleFormSubmit} = useForm({defaultValues:initialValues});
+    const {title, description, content} = watch<keyof AdsProduct>(["title", "description", "content"]);
+    
+    const handelSubmit = (product: AdsProduct) => {
+        console.log(product);
     };
 
     return (
@@ -32,25 +32,25 @@ const Product = () => {
                     <img src={Phone} alt='Phone'/>
                     <div id="content">
                         {
-                            content.type === TEXT &&
-                            <div id="text"><p>{content.value || 'متن خود را وارد کنید.'}</p></div>
+                            content.type === "Text" &&
+                            <div id="text"><p>{content.value || "متن خود را وارد کنید."}</p></div>
                         }
                         {
-                            content.type === PICTURE &&
+                            content.type === "Picture" &&
                             (content.value ?
                              <img src={content.value} alt='content'/> :
                              <FontAwesomeIcon icon={faImage}/>)
                         }
                         {
-                            content.type === VIDEO &&
+                            content.type === "Video" &&
                             (content.value ?
-                             <video src={content.value} controls/> :
+                             <video src={content.value} controls autoPlay/> :
                              <FontAwesomeIcon icon={faVideo}/>)
                         }
                     </div>
                     <div id="detail">
-                        <p>{title || 'عنوان'}</p>
-                        <p>{description || 'توضیحات'}</p>
+                        <p>{title || "عنوان"}</p>
+                        <p>{description || "توضیحات"}</p>
                     </div>
                 </div>
             </div>
@@ -61,7 +61,7 @@ const Product = () => {
                 <form onSubmit={handleFormSubmit(handelSubmit)}>
                     <TextInput name='title' register={register} placeholder='عنوان'/>
                     <TextInput name='description' register={register} placeholder='توضیحات' lines={5}/>
-                    <ContentInput register={register} setValue={setValue} contentType={content.type}/>
+                    <ContentInput register={register} setValue={setValue}/>
                     <input type="submit" value="ایجاد"/>
                 </form>
             </div>
