e62ab4036c91571f4181777b35d2c83a8e4b51c1
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-19 03:00:43 +0430

    Add a test to AccountController.RegisterUser for "OnCallWithInvalidModel" scenario.

--- ArQr.test/Controllers/AccountControllerTests/RegisterUserAction.cs
+++ ArQr.test/Controllers/AccountControllerTests/RegisterUserAction.cs
@@ -45,5 +45,27 @@ namespace ArQr.test.Controllers.AccountControllerTests
             Assert.True(userResource.Email    == registerUserResource.Email &&
                         userResource.UserName == registerUserResource.Email);
         }
+
+        [Theory]
+        [InlineData("DuplicateUserName")]
+        [InlineData("PasswordRequiresDigit")]
+        [InlineData("PasswordRequiresLower")]
+        public async Task OnCallWithInvalidModel_ReturnNullAsData(string errorCode)
+        {
+            UserManager
+                .Setup(manager => manager.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Failed(new IdentityError {Code = errorCode}));
+
+            var controller = new AccountController(UserManager.Object, Mapper);
+
+            var registerUserResource =
+                new RegisterUserResource {Email = "LATTERLY_ANY_EMAIL", Password = "LATTERLY_ANY_PASSWORD"};
+
+            var controllerResult = (BadRequestObjectResult) await controller.RegisterUser(registerUserResource);
+            var apiResponse      = (ApiResponse) controllerResult.Value;
+            var userResource     = (UserResource) apiResponse.Data;
+
+            Assert.Null(userResource);
+        }
     }
 }
