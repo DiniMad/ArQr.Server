f639b6b6d5907b2b1e6c5569250a27be057b854e
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-19 21:52:48 +0430

    Create the AcceptLanguageHeaderCultureProvider to implement the RequestCultureProvider for extracting culture from the Accept-Language header.

---
+++ ArQr/Infrastructure/AcceptLanguageHeaderCultureProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+
+namespace ArQr.Infrastructure
+{
+    public class AcceptLanguageHeaderCultureProvider : RequestCultureProvider
+    {
+        public override async Task<ProviderCultureResult> DetermineProviderCultureResult(HttpContext httpContext)
+        {
+            var userLanguages = httpContext.Request.Headers["Accept-Language"].ToString();
+            var firstLanguage = userLanguages.Split(',').FirstOrDefault();
+            return !string.IsNullOrWhiteSpace(firstLanguage)
+                       ? new ProviderCultureResult(firstLanguage, firstLanguage)
+                       : default;
+        }
+    }
+}
