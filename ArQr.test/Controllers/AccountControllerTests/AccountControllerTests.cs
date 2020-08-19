268a6b79e058029b7cb7d241c2bee844e5f7b9a1
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-19 22:08:20 +0430

    Create a mock for localizer in the AccountControllerTests class.

--- ArQr.test/Controllers/AccountControllerTests/AccountControllerTests.cs
+++ ArQr.test/Controllers/AccountControllerTests/AccountControllerTests.cs
@@ -1,8 +1,9 @@
 using ArQr.Infrastructure;
+using ArQr.Localization;
 using ArQr.Models;
 using AutoMapper;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Localization;
 using Moq;
 
 namespace ArQr.test.Controllers.AccountControllerTests
@@ -11,15 +12,18 @@ namespace ArQr.test.Controllers.AccountControllerTests
     {
         protected readonly Mock<UserManager<ApplicationUser>> UserManager;
         protected readonly IMapper                            Mapper;
+        protected readonly Mock<IStringLocalizer<Resource>>   Localizer;
 
         protected AccountControllerTests()
         {
             var store = new Mock<IUserStore<ApplicationUser>>();
             UserManager =
                 new Mock<UserManager<ApplicationUser>>(store.Object, null, null, null, null, null, null, null, null);
-            
+
             var mockMapper = new MapperConfiguration(config => config.AddProfile(new AutoMapperProfile()));
             Mapper = mockMapper.CreateMapper();
+
+            Localizer = new Mock<IStringLocalizer<Resource>>();
         }
     }
 }
