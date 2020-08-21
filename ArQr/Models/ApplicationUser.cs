c972827489ad19e4d0d19c34e832fe5338ea3a17
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-21 23:00:37 +0430

    Add a list of products to the application user.

--- ArQr/Models/ApplicationUser.cs
+++ ArQr/Models/ApplicationUser.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 
 namespace ArQr.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public List<Product> Products { get; set; }
+
+        public ApplicationUser()
+        {
+            Products = new List<Product>();
+        }
     }
 }
