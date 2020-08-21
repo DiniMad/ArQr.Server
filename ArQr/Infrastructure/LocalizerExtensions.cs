71182e1617ad656cd3dde739fc3da1c417567516
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-21 23:05:51 +0430

    Made a slight refactoring into the localizer extensions class.

--- ArQr/Infrastructure/LocalizerExtensions.cs
+++ ArQr/Infrastructure/LocalizerExtensions.cs
@@ -5,9 +5,6 @@ namespace ArQr.Infrastructure
     public static class LocalizerExtensions
     {
         public static string GetUserManagerCreateError(this IStringLocalizer localizer, string errorCode)
-        {
-            var key = $"UserManagerCreate-{errorCode}";
-            return localizer[key];
-        }
+            => localizer[$"UserManagerCreate-{errorCode}"];
     }
 }
