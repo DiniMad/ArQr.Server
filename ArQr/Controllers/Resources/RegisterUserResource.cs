2aeb6e7dd328b9f156a625ee55d405a23c1f157b
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-21 23:23:30 +0430

    Add the required data annotation for the both register user resource properties.

--- ArQr/Controllers/Resources/RegisterUserResource.cs
+++ ArQr/Controllers/Resources/RegisterUserResource.cs
@@ -4,9 +4,11 @@ namespace ArQr.Controllers.Resources
 {
     public class RegisterUserResource
     {
+        [Required]
         [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
         [DataType(DataType.Password)]
         [StringLength(64, MinimumLength = 6)]
         public string Password { get; set; }
