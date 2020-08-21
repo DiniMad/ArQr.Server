7b98a9f71a52055492cb30d822c64af04d19601f
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-21 22:59:25 +0430

    Add the migration files for create the Product table.

--- ArQr/Data/Migrations/20200820185800_CreateTheProductTable..Designer.cs
+++ ArQr/Data/Migrations/20200820185800_CreateTheProductTable..Designer.cs
@@ -3,14 +3,16 @@ using System;
 using ArQr.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ArQr.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200820185800_CreateTheProductTable.")]
+    partial class CreateTheProductTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
