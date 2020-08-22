5f45c01099e17232bf50b6ba8ff28c92ad3cf2d3
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-22 22:36:07 +0430

    Create a sub class of product controller tests for get product action with a method to test the "GetTheProductBelongsToTheUser" scenario.

---
+++ ArQr.test/Controllers/ProductControllerTests/GetProductAction.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using ArQr.Controllers;
+using ArQr.Controllers.Resources;
+using ArQr.Models;
+using ArQr.Models.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+using static ArQr.test.UtilityMethods;
+
+namespace ArQr.test.Controllers.ProductControllerTests
+{
+    public class GetProductAction : ProductControllerTests
+    {
+        private readonly Mock<IProductRepository> _productRepository;
+
+        public GetProductAction()
+        {
+            _productRepository = new Mock<IProductRepository>();
+        }
+
+        [Fact]
+        public async Task GetTheProductBelongsToTheUser_ReturnProduct()
+        {
+            const string productId = "LITERALLY_ANY_PRODUCT_ID";
+            const string userId    = "LITERALLY_ANY_USER_ID";
+
+            var product = new Product
+            {
+                Id      = productId,
+                OwnerId = userId
+            };
+            _productRepository
+                .Setup(repository => repository.GetAsync(productId))
+                .ReturnsAsync(product);
+
+            UnitOfWork
+                .Setup(unitOfWork => unitOfWork.Products)
+                .Returns(_productRepository.Object);
+
+
+            var controller = new ProductController(UnitOfWork.Object, Mapper, Localizer.Object)
+            {
+                ControllerContext = {HttpContext = CreateHttpContext(userId)}
+            };
+
+            var controllerResult = (OkObjectResult) await controller.GetProduct(productId);
+            var apiResponse      = (ApiResponse) controllerResult.Value;
+
+            Assert.NotNull((ProductResource) apiResponse.Data);
+        }
+    }
+}
