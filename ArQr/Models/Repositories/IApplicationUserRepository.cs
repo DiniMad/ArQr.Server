cc6c9cce49e7308f0f5dbd05ed273682710edaf9
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-22 22:18:04 +0430

    Modify the application user repository interface to inherit from the repository interface and remove its redundant method deceleration.

--- ArQr/Models/Repositories/IApplicationUserRepository.cs
+++ ArQr/Models/Repositories/IApplicationUserRepository.cs
@@ -1,9 +1,7 @@
-using System.Threading.Tasks;
 
 namespace ArQr.Models.Repositories
 {
-    public interface IApplicationUserRepository
+    public interface IApplicationUserRepository : IRepository<ApplicationUser>
     {
-        Task<ApplicationUser> GetUserAsync(string userId);
     }
 }
