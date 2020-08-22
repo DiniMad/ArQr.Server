21596e26f9fb54b9f493fbabff895ebc1f0ac5b5
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-23 00:06:07 +0430

    Create a test in the ProductControllerTests.CreateProductAction for the "Always_ReturnProductWithValidData" scenario.

--- ArQr.test/Controllers/ProductControllerTests/CreateProductAction.cs
+++ ArQr.test/Controllers/ProductControllerTests/CreateProductAction.cs
@@ -71,5 +71,45 @@ namespace ArQr.test.Controllers.ProductControllerTests
 
             UnitOfWork.Verify(unitOfWork => unitOfWork.Complete(), Times.Once);
         }
+
+        [Fact]
+        public async Task Always_ReturnProductWithValidData()
+        {
+            const string      productTitle       = "LITERALLY_ANY_PRODUCT_TITLE";
+            const string      productDescription = "LITERALLY_ANY_PRODUCT_DESCRIPTION";
+            const ProductType productType        = ProductType.Picture;
+            const string      productContent     = "LITERALLY_ANY_PRODUCT_CONTENT";
+
+            var productResource = new ProductResource
+            {
+                Title       = productTitle,
+                Description = productDescription,
+                Type        = productType,
+                Content     = productContent
+            };
+
+            UnitOfWork
+                .Setup(unitOfWork => unitOfWork.Products)
+                .Returns(_productRepository.Object);
+
+            _urlHelper
+                .Setup(urlHelper => urlHelper.Action(It.IsAny<UrlActionContext>()))
+                .Returns(string.Empty);
+
+            var controller = new ProductController(UnitOfWork.Object, Mapper, Localizer.Object)
+            {
+                ControllerContext = {HttpContext = CreateHttpContext()},
+                Url               = _urlHelper.Object
+            };
+
+            var controllerResult        = (CreatedResult) await controller.CreateProduct(productResource);
+            var apiResponse             = (ApiResponse) controllerResult.Value;
+            var responseProductResource = (ProductResource) apiResponse.Data;
+
+            Assert.True(responseProductResource.Title       == productTitle       &&
+                        responseProductResource.Description == productDescription &&
+                        responseProductResource.Type        == productType        &&
+                        responseProductResource.Content     == productContent);
+        }
     }
 }
