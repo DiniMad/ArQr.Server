e9c5fe4f057a8537c70ad105ba4b690bafa51268
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-25 12:24:44 +0430

    Config any CORS.

--- ArQr.FileManagement/Startup.cs
+++ ArQr.FileManagement/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace ArQr.FileManagement
 {
@@ -25,6 +18,9 @@ namespace ArQr.FileManagement
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddCors(o => o.AddPolicy("MyPolicy",
+                                              builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -37,6 +33,8 @@ namespace ArQr.FileManagement
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseCors("MyPolicy");
 
             app.UseAuthorization();
 
