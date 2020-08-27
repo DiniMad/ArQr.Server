3930331888ba72c49fd7332c2a4959f183fca13b
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-08-27 05:10:24 +0430

    Create two extension methods to get image size and video size from the configuration.

---
+++ ArQr.FileManagement/Infrastructure/ConfigurationExtension.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.Configuration;
+
+namespace ArQr.FileManagement.Infrastructure
+{
+    public static class ConfigurationExtension
+    {
+        private const int MbCoefficient = 1024 * 1024;
+
+        public static long GetImageMaxSizeInByte(this IConfiguration configuration)
+            => long.Parse(configuration["ImageMaxSizeInMb"]) * MbCoefficient;
+
+        public static long GetVideoMaxSizeInByte(this IConfiguration configuration)
+            => long.Parse(configuration["VideoMaxSizeInMb"]) * MbCoefficient;
+    }
+}
