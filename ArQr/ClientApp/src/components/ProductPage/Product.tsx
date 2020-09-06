817c80a41693f6423443f9f4c17ab8122805f2d9
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-09-06 16:11:58 +0430

    Add functions for handling upload media and add product.

--- ArQr/ClientApp/src/components/ProductPage/Product.tsx
+++ ArQr/ClientApp/src/components/ProductPage/Product.tsx
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from "react";
 import {useForm} from "react-hook-form";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faImage, faVideo} from "@fortawesome/free-solid-svg-icons";
+import * as Yup from "yup";
+import {yupResolver} from "@hookform/resolvers";
 
 import TextInput from "./TextInput";
 import ContentInput from "./ContentInput";
@@ -9,8 +11,9 @@ import Modal from "../Modal";
 import UploadMediaPopup from "./UploadMediaPopup";
 import useAuthenticatedHttp from "../hooks/useAuthenticatedHttp";
 import {httpStatusCode, urls} from "../services/constants";
-import * as Yup from "yup";
-import {yupResolver} from "@hookform/resolvers";
+import {blobUrlToFile, objectToFormData} from "../services/file";
+import {AdsProduct, MediaConfiguration, ProductContentType, UseState} from "../types";
+import {Phone} from "../../images";
 
 const Product = () => {
     const initialValues: AdsProduct = {
@@ -51,7 +54,7 @@ const Product = () => {
         errors
     } = useForm({defaultValues: initialValues, resolver: yupResolver(validationSchema)});
     const {title, description, content} = watch<keyof AdsProduct>(["title", "description", "content"]);
-    const {get} = useAuthenticatedHttp();
+    const {get, post} = useAuthenticatedHttp();
 
     useEffect(() => {
         fetchConfiguration();
@@ -67,8 +70,70 @@ const Product = () => {
         setMediaConfiguration(configuration);
     };
 
-    const handelSubmit = (product: AdsProduct) => {
-        console.log(product);
+    const convertToEnum = (type: ProductContentType) => {
+        switch (type) {
+            case "Text":
+                return 0;
+            case "Picture":
+                return 1;
+            case "Video":
+                return 2;
+        }
+    };
+    const convertToServerModel = (product: AdsProduct) => {
+        return {
+            title      : product.title,
+            description: product.description,
+            type       : convertToEnum(product.content.type),
+            Content    : product.content.value
+        };
+    };
+    const addProduct = async (product: AdsProduct) => {
+        const data = convertToServerModel(product);
+        const response = await post(urls.apis.product, data);
+        if (response.status === httpStatusCode.success) {
+            // TODO: Display a notification
+        }
+    };
+
+    const uploadTextProduct = async (product: AdsProduct) => {
+        await addProduct(product);
+    };
+    const uploadMediaProduct = async (product: AdsProduct) => {
+        setModalVisibility(true);
+        const file = await blobUrlToFile(product.content.value);
+        const form = objectToFormData({fileMime: file.type, fileSize: file.size});
+        const {status, statusText, data: sessionId} = await post<string>(urls.fileManagement.createSession, form);
+        if (status !== httpStatusCode.success) {
+            console.error(statusText);
+            return;
+        }
+        setMediaInfo({file, sessionId});
+    };
+
+    const handelSubmit = async (product: AdsProduct) => {
+        if (content.type === "Text") await uploadTextProduct(product);
+        else await uploadMediaProduct(product);
+    };
+
+    const endTheUploadSession = async () => {
+        const data = objectToFormData({sessionId: mediaInfo?.sessionId});
+        const response = await post(urls.fileManagement.end, data);
+        return response.status === httpStatusCode.success;
+    };
+
+    const handleUploadMediaCompleted = async () => {
+        setModalVisibility(false);
+        if (!await endTheUploadSession()) {
+            // TODO: Display a notification
+            return;
+        }
+        const product: AdsProduct = {
+            title,
+            description,
+            content: {type: content.type, value: mediaInfo!.sessionId}
+        };
+        await addProduct(product);
     };
     const handleUploadMediaCanceled = async () => {
         // TODO: Display a notification
