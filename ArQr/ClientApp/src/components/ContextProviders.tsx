ce7282ab85b2317de1f55df2f5c96dd5c6e3b2ff
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-09-16 19:00:57 +0430

    Add the ToastProvider to the ContextProviders component.

--- ArQr/ClientApp/src/components/ContextProviders.tsx
+++ ArQr/ClientApp/src/components/ContextProviders.tsx
@@ -1,16 +1,19 @@
 import React, {ReactChild} from "react";
+import {ToastProvider} from "react-toast-notifications";
 
-import AccessTokenProvider from './contexts/AccessTokenContext';
-import UserProvider from './contexts/UserContext';
+import AccessTokenProvider from "./contexts/AccessTokenContext";
+import UserProvider from "./contexts/UserContext";
 
 type Props = {
     children: ReactChild;
 }
-const ContextProviders = ({children}:Props) => {
+const ContextProviders = ({children}: Props) => {
     return (
         <AccessTokenProvider>
             <UserProvider>
-                {children}
+                <ToastProvider autoDismiss autoDismissTimeout={5000}>
+                    {children}
+                </ToastProvider>
             </UserProvider>
         </AccessTokenProvider>
     );
