78f0043db036dad5ab43fc10a152e830b01761e6
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-14 20:28:29 +0330

    Create the DesignTimeDbContextFactory class for creating an instance of ApplicationDbContext.

---
+++ Source/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Data
+{
+    class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+            var connectionString = configuration.GetConnectionString("Default");
+
+            var optionBuilder = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlServer(connectionString);
+            return new ApplicationDbContext(optionBuilder.Options);
+        }
+    }
+}
