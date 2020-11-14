b64bc83302709f730a7ff558ded14ce479bc9307
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-15 00:36:24 +0330

    Add required properties to the User domain.

--- Source/Domain/User.cs
+++ Source/Domain/User.cs
@@ -4,6 +4,10 @@ namespace Domain
 {
     public class User : BaseDomain
     {
-
+        public string Phone          { get; set; }
+        public string PasswordHash   { get; set; }
+        public string Email          { get; set; }
+        public bool   EmailConfirmed { get; set; }
+        public bool   PhoneConfirmed { get; set; }
     }
 }
