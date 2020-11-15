2604c03bbcbd1879ea19cb6721089b568cf54056
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-15 04:03:57 +0330

    Add migration files for renaming PhoneConfirmed column to PhoneNumnerConfirmed.

---
+++ Source/Data/Migrations/20201115003156_RenameThePhoneConfirmedColumnToPhoneNumberConfirmed.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class RenameThePhoneConfirmedColumnToPhoneNumberConfirmed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PhoneConfirmed",
+                table: "Users",
+                newName: "PhoneNumberConfirmed");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PhoneNumberConfirmed",
+                table: "Users",
+                newName: "PhoneConfirmed");
+        }
+    }
+}
