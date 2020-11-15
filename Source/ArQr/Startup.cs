13b858d9e1324e4e3d0ca2ac6360e7d4048382a9
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-15 03:45:39 +0330

    Register the UnitOfWork using a custom extension method.

--- Source/ArQr/Startup.cs
+++ Source/ArQr/Startup.cs
@@ -1,6 +1,6 @@
+using ArQr.Helper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +19,8 @@ namespace ArQr
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddUnitOfWork(Configuration.GetConnectionString("Default"));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
