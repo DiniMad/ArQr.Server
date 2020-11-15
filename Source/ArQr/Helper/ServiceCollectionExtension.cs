d251ecb936debcdfa56e4aef41e3834d996826b3
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-15 03:44:45 +0330

    Create a helper class for service collection extension methods.

---
+++ Source/ArQr/Helper/ServiceCollectionExtension.cs
@@ -0,0 +1,14 @@
+using Data.Repository;
+using Data.Repository.Base;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ArQr.Helper
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddUnitOfWork(this IServiceCollection services, string connectionString)
+        {
+            return services.AddScoped<IUnitOfWork, UnitOfWork>(_ => new UnitOfWork(connectionString));
+        }
+    }
+}
