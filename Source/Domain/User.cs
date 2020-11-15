337dc047fd540ba1402a90e312ddd69a9649d934
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-15 04:03:22 +0330

    Rename the PhoneConfirmed property of the User domain to "PhoneNumberConfirmed".

--- Source/Domain/User.cs
+++ Source/Domain/User.cs
@@ -4,10 +4,10 @@ namespace Domain
 {
     public class User : BaseDomain
     {
-        public string PhoneNumber    { get; set; }
-        public string PasswordHash   { get; set; }
-        public string Email          { get; set; }
-        public bool   EmailConfirmed { get; set; }
-        public bool   PhoneConfirmed { get; set; }
+        public string PhoneNumber          { get; set; }
+        public string PasswordHash         { get; set; }
+        public string Email                { get; set; }
+        public bool   EmailConfirmed       { get; set; }
+        public bool   PhoneNumberConfirmed { get; set; }
     }
 }
