da8c897030e1dddd879e1e197111449aa04dee07
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-16 04:54:56 +0330

    Create a extension method for the ServiceCollection type to register the JWT authentication.

--- Source/ArQr/Helper/ServiceCollectionExtension.cs
+++ Source/ArQr/Helper/ServiceCollectionExtension.cs
@@ -1,6 +1,9 @@
+using System.Text;
 using Data.Repository;
 using Data.Repository.Base;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ArQr.Helper
 {
@@ -9,6 +12,21 @@ namespace ArQr.Helper
         public static IServiceCollection AddUnitOfWork(this IServiceCollection services, string connectionString)
         {
             return services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, string signingKey)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer           = false,
+                        ValidateAudience         = false,
+                        ValidateLifetime         = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey         = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey))
+                    });
+
+            return services;
         }
     }
 }
