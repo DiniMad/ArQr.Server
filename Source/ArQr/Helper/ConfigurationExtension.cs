150e03f329388304ac83f97ac8cf04c324464252
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-18 11:02:36 +0330

    Remove the redundant GetJwtSigningKey extension method.

--- Source/ArQr/Helper/ConfigurationExtension.cs
+++ Source/ArQr/Helper/ConfigurationExtension.cs
@@ -5,11 +5,6 @@ namespace ArQr.Helper
 {
     public static class ConfigurationExtension
     {
-        public static string GetJwtSigningKey(this IConfiguration configuration)
-        {
-            return configuration.GetValue<string>("JwtSigningKey");
-        }
-
         public static TokenOption GetTokenOption(this IConfiguration configuration)
         {
             return configuration.GetSection("Token").Get<TokenOption>();
