35b801a60750c622ad9ce34ade6ba94e92aafe19
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-18 09:30:50 +0330

    Add a property of type of UserRefreshToken to the User domain.

--- Source/Domain/User.cs
+++ Source/Domain/User.cs
@@ -4,10 +4,11 @@ namespace Domain
 {
     public class User : BaseDomain
     {
-        public string PhoneNumber          { get; set; }
-        public string PasswordHash         { get; set; }
-        public string Email                { get; set; }
-        public bool   EmailConfirmed       { get; set; }
-        public bool   PhoneNumberConfirmed { get; set; }
+        public string           PhoneNumber          { get; set; }
+        public string           PasswordHash         { get; set; }
+        public string           Email                { get; set; }
+        public bool             EmailConfirmed       { get; set; }
+        public bool             PhoneNumberConfirmed { get; set; }
+        public UserRefreshToken RefreshToken         { get; set; }
     }
 }
