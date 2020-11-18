49c597577f9c2087ec9c1d92fcae0a9587706893
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-18 13:42:24 +0330

    Change the user register validation to accept phone number as a string with length of 10.

--- Source/Resource/Api/Validations/UserRegisterValidation.cs
+++ Source/Resource/Api/Validations/UserRegisterValidation.cs
@@ -7,7 +7,7 @@ namespace Resource.Api.Validations
     {
         public UserRegisterValidation()
         {
-            RuleFor(resource => resource.PhoneNumber).NotNull().NotEmpty().Length(11);
+            RuleFor(resource => resource.PhoneNumber).NotNull().NotEmpty().Length(10);
             RuleFor(resource => resource.Password).NotNull().Matches(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$");
         }
     }
