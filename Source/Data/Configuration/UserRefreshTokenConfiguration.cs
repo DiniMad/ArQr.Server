dcfe26b3ce8249321e678794a59c82a8c589dfa6
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-18 10:07:02 +0330

    Add a configuration class for UserRefreshToken domain.

---
+++ Source/Data/Configuration/UserRefreshTokenConfiguration.cs
@@ -0,0 +1,15 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.Configuration
+{
+    public class UserRefreshTokenConfiguration : IEntityTypeConfiguration<UserRefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<UserRefreshToken> builder)
+        {
+            builder.Property(refreshToken => refreshToken.ExpireDate)
+                   .HasColumnType("date");
+        }
+    }
+}
