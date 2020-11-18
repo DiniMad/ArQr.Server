9f65726f1a2463615c683d760d6ca41381fc64b3
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-18 10:59:22 +0330

    Add a method to the TokenOption class to return the security key.

--- Source/ArQr/Model/TokenOption.cs
+++ Source/ArQr/Model/TokenOption.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
 namespace ArQr.Model
 {
     public class TokenOption
@@ -5,5 +8,10 @@ namespace ArQr.Model
         public string JwtSigningKey                    { get; set; }
         public int    JwtExpireIntervalInMinutes       { get; set; }
         public int    RefreshTokenExpireIntervalInDays { get; set; }
+
+        public SymmetricSecurityKey GetSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtSigningKey));
+        }
     }
 }
