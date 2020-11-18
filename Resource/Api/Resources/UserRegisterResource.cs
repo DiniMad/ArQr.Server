9dba624af40aea6c78fa984d3b060a7dd30eaa97
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-18 13:08:11 +0330

    Create resource file for user register.

---
+++ Resource/Api/Resources/UserRegisterResource.cs
@@ -0,0 +1,8 @@
+namespace Resource.Api.Resources
+{
+    public class UserRegisterResource
+    {
+        public string PhoneNumber { get; set; }
+        public string Password    { get; set; }
+    }
+}
