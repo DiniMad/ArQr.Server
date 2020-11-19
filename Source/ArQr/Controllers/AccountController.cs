f2af166469374b1c04d3f9b653729b8df470c84c
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-19 08:55:13 +0330

    Add an action to the account controller for user login.

--- Source/ArQr/Controllers/AccountController.cs
+++ Source/ArQr/Controllers/AccountController.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
+using ArQr.Helper;
+using ArQr.Interface;
 using AutoMapper;
 using Data.Repository.Base;
 using Domain;
@@ -10,19 +13,23 @@ using Resource.Api.Resources;
 
 namespace ArQr.Controllers
 {
+
     [ApiController]
     [Route("Account")]
     public class AccountController : ControllerBase
     {
         private readonly IUnitOfWork           _unitOfWork;
+        private readonly ITokenService         _tokenService;
         private readonly IMapper               _mapper;
         private readonly IPasswordHasher<User> _passwordHasher;
 
         public AccountController(IUnitOfWork           unitOfWork,
+                                 ITokenService         tokenService,
                                  IMapper               mapper,
                                  IPasswordHasher<User> passwordHasher)
         {
             _unitOfWork     = unitOfWork;
+            _tokenService   = tokenService;
             _mapper         = mapper;
             _passwordHasher = passwordHasher;
         }
@@ -46,6 +53,36 @@ namespace ArQr.Controllers
             }
 
             return Created("", user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserLoginResource loginResource)
+        {
+            var searchResult =
+                await _unitOfWork.UserRepository
+                                 .FindAsync(u => u.PhoneNumber == loginResource.PhoneNumber);
+            var user = searchResult.ToList().FirstOrDefault();
+            if (user is null) return NotFound();
+
+            try
+            {
+                var isPasswordValid = _passwordHasher.VerifyHashedPassword(user,
+                                                                           user.PasswordHash,
+                                                                           loginResource.Password);
+                if (isPasswordValid == PasswordVerificationResult.Failed) return BadRequest("Wrong Password");
+            }
+            catch (FormatException)
+            {
+                return BadRequest("Wrong Password");
+            }
+
+            var refreshToken = _tokenService.GenerateRefreshToken();
+            user.RefreshToken = refreshToken;
+            _unitOfWork.UserRepository.Update(user);
+            await _unitOfWork.CompleteAsync();
+
+            var jwtToken = _tokenService.GenerateJwtToken(user.GetClaims());
+            return Ok(jwtToken);
         }
     }
 }
