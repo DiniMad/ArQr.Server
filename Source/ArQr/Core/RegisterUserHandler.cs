bf9a63475ac71cd4388bd125b33fd8a9ec761a67
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-19 11:42:09 +0330

    Create a file for RegisterUserHandler.

---
+++ Source/ArQr/Core/RegisterUserHandler.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Data.Repository.Base;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
+using Resource.Api.Resources;
+
+namespace ArQr.Core
+{
+    public sealed record RegisterUserRequest(UserRegisterResource RegisterResource) : IRequest<RegisterUserResult>;
+
+    public sealed record RegisterUserResult(int StatusCode, object Value);
+
+    public class RegisterUserHandler : IRequestHandler<RegisterUserRequest, RegisterUserResult>
+    {
+        private readonly IMapper               _mapper;
+        private readonly IPasswordHasher<User> _passwordHasher;
+        private readonly IUnitOfWork           _unitOfWork;
+
+        public RegisterUserHandler(IMapper mapper, IPasswordHasher<User> passwordHasher, IUnitOfWork unitOfWork)
+        {
+            _mapper         = mapper;
+            _passwordHasher = passwordHasher;
+            _unitOfWork     = unitOfWork;
+        }
+
+        public async Task<RegisterUserResult> Handle(RegisterUserRequest request, CancellationToken cancellationToken)
+        {
+            var registerResource = request.RegisterResource;
+            var user             = _mapper.Map<User>(registerResource);
+            user.PasswordHash = _passwordHasher.HashPassword(user, registerResource.Password);
+
+            await _unitOfWork.UserRepository.InsertAsync(user);
+            try
+            {
+                await _unitOfWork.CompleteAsync();
+            }
+            catch (Exception e)
+            {
+                return (e.InnerException as SqlException)?.Number == 2601
+                           ? new(StatusCodes.Status409Conflict, "Phone Number already taken.")
+                           : new(StatusCodes.Status500InternalServerError, "Unhandled Exception.");
+            }
+
+            return new(StatusCodes.Status201Created, user);
+        }
+    }
+}
