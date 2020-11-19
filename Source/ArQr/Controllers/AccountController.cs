418101257b80ca32b009c77d98bd2e64a107e04a
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-19 12:01:53 +0330

    Update the AccountController to user the login user handler.

--- Source/ArQr/Controllers/AccountController.cs
+++ Source/ArQr/Controllers/AccountController.cs
@@ -1,14 +1,8 @@
-using System;
 using System.Threading.Tasks;
 using ArQr.Core;
-using ArQr.Helper;
-using ArQr.Interface;
-using AutoMapper;
-using Data.Repository.Base;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Resource.Api.Resources;
 
@@ -18,20 +12,11 @@ namespace ArQr.Controllers
     [Route("Account")]
     public class AccountController : ControllerBase
     {
-        private readonly IUnitOfWork           _unitOfWork;
-        private readonly ITokenService         _tokenService;
-        private readonly IPasswordHasher<User> _passwordHasher;
-        private readonly IMediator             _mediator;
+        private readonly IMediator _mediator;
 
-        public AccountController(IUnitOfWork           unitOfWork,
-                                 ITokenService         tokenService,
-                                 IPasswordHasher<User> passwordHasher,
-                                 IMediator             mediator)
+        public AccountController(IMediator mediator)
         {
-            _unitOfWork     = unitOfWork;
-            _tokenService   = tokenService;
-            _passwordHasher = passwordHasher;
-            _mediator       = mediator;
+            _mediator = mediator;
         }
 
         [HttpPost("register")]
@@ -44,28 +29,8 @@ namespace ArQr.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<string>> Login(UserLoginResource loginResource)
         {
-            var user = await _unitOfWork.UserRepository.GetAsync(loginResource.PhoneNumber);
-            if (user is null) return NotFound();
-
-            try
-            {
-                var isPasswordValid = _passwordHasher.VerifyHashedPassword(user,
-                                                                           user.PasswordHash,
-                                                                           loginResource.Password);
-                if (isPasswordValid == PasswordVerificationResult.Failed) return BadRequest("Wrong Password");
-            }
-            catch (FormatException)
-            {
-                return BadRequest("Wrong Password");
-            }
-
-            var refreshToken = _tokenService.GenerateRefreshToken();
-            user.RefreshToken = refreshToken;
-            _unitOfWork.UserRepository.Update(user);
-            await _unitOfWork.CompleteAsync();
-
-            var jwtToken = _tokenService.GenerateJwtToken(user.GetClaims());
-            return Ok(jwtToken);
+            var (statusCode, value) = await _mediator.Send(new LoginUserRequest(loginResource));
+            return StatusCode(statusCode, value);
         }
     }
 }
