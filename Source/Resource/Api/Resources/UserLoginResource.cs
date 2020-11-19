79ebf8527ee5afdacb18fb19424bf39ee284f331
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-19 09:13:09 +0330

    Change type of the UserLoginResource to record.

--- Source/Resource/Api/Resources/UserLoginResource.cs
+++ Source/Resource/Api/Resources/UserLoginResource.cs
@@ -1,8 +1,4 @@
 namespace Resource.Api.Resources
 {
-    public class UserLoginResource
-    {
-        public string PhoneNumber { get; set; }
-        public string Password    { get; set; }
-    }
+    public record UserLoginResource(string PhoneNumber, string Password);
 }
