350a13dee4961758ba2c821d00d4e89825e2922c
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-19 08:53:46 +0330

    Create a resource file for user login.

---
+++ Source/Resource/Api/Resources/UserLoginResource.cs
@@ -0,0 +1,8 @@
+namespace Resource.Api.Resources
+{
+    public class UserLoginResource
+    {
+        public string PhoneNumber { get; set; }
+        public string Password    { get; set; }
+    }
+}
