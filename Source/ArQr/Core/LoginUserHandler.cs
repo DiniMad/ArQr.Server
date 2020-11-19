90181453cf3a02ec4be0725ac0870c2ce7ea294c
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-19 12:01:13 +0330

    Create a file for LoginUserHandler.

---
+++ Source/ArQr/Core/LoginUserHandler.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ArQr.Helper;
+using ArQr.Interface;
+using Data.Repository.Base;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Resource.Api.Resources;
+
+namespace ArQr.Core
+{
+    public sealed record LoginUserRequest(UserLoginResource LoginResource) : IRequest<LoginUserResult>;
+
+    public sealed record LoginUserResult(int StatusCode, object Value);
+
+    public class LoginUserHandler : IRequestHandler<LoginUserRequest, LoginUserResult>
+    {
+        private readonly IUnitOfWork           _unitOfWork;
+        private readonly IPasswordHasher<User> _passwordHasher;
+        private readonly ITokenService         _tokenService;
+
+        public LoginUserHandler(IUnitOfWork           unitOfWork,
+                                IPasswordHasher<User> passwordHasher,
+                                ITokenService         tokenService)
+        {
+            _unitOfWork     = unitOfWork;
+            _passwordHasher = passwordHasher;
+            _tokenService   = tokenService;
+        }
+
+        public async Task<LoginUserResult> Handle(LoginUserRequest request, CancellationToken cancellationToken)
+        {
+            var loginResource = request.LoginResource;
+            var user          = await _unitOfWork.UserRepository.GetAsync(loginResource.PhoneNumber);
+            if (user is null) return new(StatusCodes.Status404NotFound, "User Not Found.");
+
+            try
+            {
+                var isPasswordValid = _passwordHasher.VerifyHashedPassword(user,
+                                                                           user.PasswordHash,
+                                                                           loginResource.Password);
+                if (isPasswordValid == PasswordVerificationResult.Failed)
+                    return new(StatusCodes.Status404NotFound, "Wrong Password.");
+            }
+            catch (FormatException)
+            {
+                return new(StatusCodes.Status404NotFound, "Wrong Password.");
+            }
+
+            var refreshToken = _tokenService.GenerateRefreshToken();
+            user.RefreshToken = refreshToken;
+            _unitOfWork.UserRepository.Update(user);
+            await _unitOfWork.CompleteAsync();
+
+            var jwtToken = _tokenService.GenerateJwtToken(user.GetClaims());
+            return new(StatusCodes.Status200OK, jwtToken);
+        }
+    }
+}
