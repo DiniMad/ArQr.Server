8bb376db74692381457eb6d0cfb85c94509e5cc6
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-20 07:10:36 +0330

    Create a record and an extension method to return UserAthentication for the HttpContext type.

--- Source/ArQr/Helper/HttpContextExtension.cs
+++ Source/ArQr/Helper/HttpContextExtension.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
 using Microsoft.IdentityModel.JsonWebTokens;
@@ -8,10 +7,22 @@ namespace ArQr.Helper
 {
     public static class HttpContextExtension
     {
+        public sealed record UserAuthentication(bool IsAuthenticated, Guid Id);
+
         public static Guid GetUserId(this HttpContext httpContext)
         {
             var userId = httpContext.User.FindFirstValue(JwtRegisteredClaimNames.Sub);
             return new Guid(userId);
+        }
+
+        public static UserAuthentication GetUserAuthentication(this HttpContext httpContext)
+        {
+            var userId = httpContext.User.FindFirstValue(JwtRegisteredClaimNames.Sub);
+
+            var isAuthenticated = userId is not null;
+            var userGuidId      = isAuthenticated ? Guid.Parse(userId!) : Guid.Empty;
+
+            return new(isAuthenticated, userGuidId);
         }
     }
 }
