bb5383ed12db5a31b4052c95a247092086649e2e
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-20 15:31:02 +0330

    Update the "RefreshTokenValidation" class to get the property names from the string localizer.

--- Source/Resource/Api/Validations/RefreshTokenValidation.cs
+++ Source/Resource/Api/Validations/RefreshTokenValidation.cs
@@ -1,14 +1,22 @@
 using FluentValidation;
+using Microsoft.Extensions.Localization;
 using Resource.Api.Resources;
+using Resource.ResourceFiles;
 
 namespace Resource.Api.Validations
 {
     public class RefreshTokenValidation : AbstractValidator<RefreshTokenResource>
     {
-        public RefreshTokenValidation()
+        public RefreshTokenValidation(IStringLocalizer<ResourcesPropertyNames> propertyNames)
         {
-            RuleFor(resource => resource.UserId).NotNull().NotEmpty();
-            RuleFor(resource => resource.RefreshToken).NotNull().NotEmpty();
+            RuleFor(resource => resource.UserId)
+                .NotNull()
+                .NotEmpty()
+                .WithName(_ => propertyNames[ResourcesPropertyNames.UserId]);
+            RuleFor(resource => resource.RefreshToken)
+                .NotNull()
+                .NotEmpty()
+                .WithName(_ => propertyNames[ResourcesPropertyNames.RefreshToken]);
         }
     }
 }
