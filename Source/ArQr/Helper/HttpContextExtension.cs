0350f93c1044b96a266c151b8b20ae4ffcb3d397
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-20 07:17:00 +0330

    Remove the redundant "GetUserId" extension method.

--- Source/ArQr/Helper/HttpContextExtension.cs
+++ Source/ArQr/Helper/HttpContextExtension.cs
@@ -9,12 +9,6 @@ namespace ArQr.Helper
     {
         public sealed record UserAuthentication(bool IsAuthenticated, Guid Id);
 
-        public static Guid GetUserId(this HttpContext httpContext)
-        {
-            var userId = httpContext.User.FindFirstValue(JwtRegisteredClaimNames.Sub);
-            return new Guid(userId);
-        }
-
         public static UserAuthentication GetUserAuthentication(this HttpContext httpContext)
         {
             var userId = httpContext.User.FindFirstValue(JwtRegisteredClaimNames.Sub);
