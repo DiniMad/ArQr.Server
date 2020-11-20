9a264f31458122b4ea24c8ff79e8597aef6abfb4
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-20 06:34:57 +0330

    Extract the claims name to constant variable for the "GetClaims" user extension method.

--- Source/ArQr/Helper/UserExtension.cs
+++ Source/ArQr/Helper/UserExtension.cs
@@ -1,19 +1,22 @@
 using System.Collections.Generic;
-using System.Security.Claims;
 using Domain;
+using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace ArQr.Helper
 {
     public static class UserExtension
     {
+        private const string ClaimNamePhoneNumber  = "phone_number";
+        private const string ClaimNameRefreshToken = "refresh_token";
+
         public static IDictionary<string, object> GetClaims(this User user)
         {
             return new Dictionary<string, object>
             {
-                {"sub", user.Id},
-                {ClaimTypes.MobilePhone, user.PhoneNumber},
-                {ClaimTypes.Email, user.Email},
-                {"refresh token", user.RefreshToken.Token}
+                {JwtRegisteredClaimNames.Sub, user.Id},
+                {JwtRegisteredClaimNames.Email, user.Email},
+                {ClaimNamePhoneNumber, user.PhoneNumber},
+                {ClaimNameRefreshToken, user.RefreshToken.Token}
             };
         }
     }
