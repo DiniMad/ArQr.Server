dd9b9ce1459b7502fd743a2bda98b27157b70b3d
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-20 08:10:17 +0330

    Modify the RefreshTokenHandler to use the mapper for updating the user refresh token.

--- Source/ArQr/Core/RefreshTokenHandler.cs
+++ Source/ArQr/Core/RefreshTokenHandler.cs
@@ -2,6 +2,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using ArQr.Helper;
 using ArQr.Interface;
+using AutoMapper;
 using Data.Repository.Base;
 using MediatR;
 using Microsoft.AspNetCore.Http;
@@ -17,11 +18,13 @@ namespace ArQr.Core
     {
         private readonly IUnitOfWork   _unitOfWork;
         private readonly ITokenService _tokenService;
+        private readonly IMapper       _mapper;
 
-        public RefreshTokenHandler(IUnitOfWork unitOfWork, ITokenService tokenService)
+        public RefreshTokenHandler(IUnitOfWork unitOfWork, ITokenService tokenService, IMapper mapper)
         {
             _unitOfWork   = unitOfWork;
             _tokenService = tokenService;
+            _mapper       = mapper;
         }
 
         public async Task<RefreshTokenResult> Handle(RefreshTokenRequest request, CancellationToken cancellationToken)
@@ -35,8 +38,7 @@ namespace ArQr.Core
             if (isRefreshTokenValid is false) return new(StatusCodes.Status401Unauthorized, "Unauthorized.");
 
             var newRefreshToken = _tokenService.GenerateRefreshToken();
-            user.RefreshToken.Token      = newRefreshToken.Token;
-            user.RefreshToken.ExpireDate = newRefreshToken.ExpireDate;
+            user.RefreshToken = _mapper.Map(newRefreshToken, user.RefreshToken);
             _unitOfWork.UserRepository.Update(user);
             await _unitOfWork.CompleteAsync();
 
