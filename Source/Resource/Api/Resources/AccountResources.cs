c6b0c550bfc03138eadb2b11cee80855e37927b3
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-20 07:52:45 +0330

    Create a resource for refreshing token.

--- Source/Resource/Api/Resources/AccountResources.cs
+++ Source/Resource/Api/Resources/AccountResources.cs
@@ -1,6 +1,9 @@
+using System;
+
 namespace Resource.Api.Resources
 {
     public record UserRegisterResource(string PhoneNumber, string Password);
 
     public record UserLoginResource(string PhoneNumber, string Password);
+    public record RefreshTokenResource(Guid UserId, string RefreshToken);
 }
