b79e7335ee7842aaefef78a135628e29588f1f7d
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-20 15:32:59 +0330

    Update the "UserRegisterValidation" class to get the property names from the string localizer.

--- Source/Resource/Api/Validations/UserRegisterValidation.cs
+++ Source/Resource/Api/Validations/UserRegisterValidation.cs
@@ -1,14 +1,23 @@
 using FluentValidation;
+using Microsoft.Extensions.Localization;
 using Resource.Api.Resources;
+using Resource.ResourceFiles;
 
 namespace Resource.Api.Validations
 {
     public class UserRegisterValidation : AbstractValidator<UserRegisterResource>
     {
-        public UserRegisterValidation()
+        public UserRegisterValidation(IStringLocalizer<ResourcesPropertyNames> propertyNames)
         {
-            RuleFor(resource => resource.PhoneNumber).NotNull().NotEmpty().Length(10);
-            RuleFor(resource => resource.Password).NotNull().Matches(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$");
+            RuleFor(resource => resource.PhoneNumber)
+                .NotNull()
+                .NotEmpty()
+                .Length(10)
+                .WithName(_ => propertyNames[ResourcesPropertyNames.PhoneNumber]);
+            RuleFor(resource => resource.Password)
+                .NotNull()
+                .Matches(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$")
+                .WithName(_ => propertyNames[ResourcesPropertyNames.Password]);
         }
     }
 }
