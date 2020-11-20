6fbf14beaf7ddd00aa7ad5a2ebdd1208d8ce6241
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-20 06:55:25 +0330

    Create an handler for user get me.

---
+++ Source/ArQr/Core/UserGetMeHandler.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ArQr.Helper;
+using AutoMapper;
+using Data.Repository.Base;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Resource.Api.Resources;
+
+namespace ArQr.Core
+{
+    public sealed record UserGetMeRequest : IRequest<UserGetMeResult>;
+
+    public sealed record UserGetMeResult(int StatusCode, object Value);
+
+    public class UserGetMeHandler : IRequestHandler<UserGetMeRequest, UserGetMeResult>
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IUnitOfWork          _unitOfWork;
+        private readonly IMapper              _mapper;
+
+        public UserGetMeHandler(IHttpContextAccessor httpContextAccessor, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _unitOfWork          = unitOfWork;
+            _mapper              = mapper;
+        }
+
+        public async Task<UserGetMeResult> Handle(UserGetMeRequest request, CancellationToken cancellationToken)
+        {
+            var userId = (_httpContextAccessor.HttpContext ?? throw new NullReferenceException()).GetUserId();
+            var user   = await _unitOfWork.UserRepository.GetAsync(userId);
+            return user is null
+                       ? new(StatusCodes.Status404NotFound, "User Not Found")
+                       : new(StatusCodes.Status200OK, _mapper.Map<UserResource>(user));
+        }
+    }
+}
