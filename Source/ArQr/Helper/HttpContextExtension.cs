42085275c47eb10c7cb775e60e6b04d3fcf36398
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-21 12:51:13 +0330

    Remove the redundant "UserAuthentication" record.

--- Source/ArQr/Helper/HttpContextExtension.cs
+++ Source/ArQr/Helper/HttpContextExtension.cs
@@ -7,8 +7,6 @@ namespace ArQr.Helper
 {
     public static class HttpContextExtension
     {
-        public sealed record UserAuthentication(bool IsAuthenticated, Guid Id);
-
         public static Guid GetUserId(this HttpContext httpContext)
         {
             var userId = httpContext.User.FindFirstValue(JwtRegisteredClaimNames.Sub);
