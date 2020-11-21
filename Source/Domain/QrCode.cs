607971d6e1fdb72be64528c1327c160b40847cfb
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-21 19:30:40 +0330

    Create a new domain for the QrCode.

---
+++ Source/Domain/QrCode.cs
@@ -0,0 +1,16 @@
+using System;
+using Domain.Base;
+
+namespace Domain
+{
+    public class QrCode : BaseDomain
+    {
+        public string  Title                 { get; set; }
+        public string  Description           { get; set; }
+        public int     CreationDate          { get; set; }
+        public int     ExpireDate            { get; set; }
+        public string? AssociatedPhoneNumber { get; set; }
+        public string? AssociatedWebsite     { get; set; }
+        public bool    Expired               => ExpireDate > DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+    }
+}
