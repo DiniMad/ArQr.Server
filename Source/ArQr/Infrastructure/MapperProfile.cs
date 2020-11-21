dea40556b165f73d60f0ee6e792aba2f2010c4a8
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-21 12:39:31 +0330

    Add a mapper for mapping user update resource to user.

--- Source/ArQr/Infrastructure/MapperProfile.cs
+++ Source/ArQr/Infrastructure/MapperProfile.cs
@@ -9,6 +9,17 @@ namespace ArQr.Infrastructure
         public MapperProfile()
         {
             CreateMap<UserRegisterResource, User>();
+            CreateMap<UserUpdateResource, User>()
+                .ForMember(user => user.EmailConfirmed,
+                           expression =>
+                               expression.MapFrom((resource, user) =>
+                                                      resource.Email is null && user.EmailConfirmed))
+                .ForMember(user => user.PhoneNumberConfirmed,
+                           expression =>
+                               expression.MapFrom((resource, user) =>
+                                                      resource.PhoneNumber is null && user.PhoneNumberConfirmed))
+                .ForAllMembers(expression =>
+                                   expression.Condition((_, _, sourceMember) => sourceMember is not null));
             CreateMap<User, UserResource>();
             CreateMap<UserRefreshToken, UserRefreshToken>()
                 .ForMember(token => token.Id, option => option.Ignore());
