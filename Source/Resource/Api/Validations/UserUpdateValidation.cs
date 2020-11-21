b8a21d0c990ea19043dde07feb6127ebfc5af94f
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-21 12:42:27 +0330

    Create a validation file for the user update resource.

---
+++ Source/Resource/Api/Validations/UserUpdateValidation.cs
@@ -0,0 +1,26 @@
+using FluentValidation;
+using FluentValidation.Validators;
+using Microsoft.Extensions.Localization;
+using Resource.Api.Resources;
+using Resource.ResourceFiles;
+
+namespace Resource.Api.Validations
+{
+    public class UserUpdateValidation : AbstractValidator<UserUpdateResource>
+    {
+        public UserUpdateValidation(IStringLocalizer<ResourcesPropertyNames>             propertyNames,
+                                    IStringLocalizer<ResourcePropertyValidationMessages> validationMessages)
+        {
+            RuleFor(resource => resource.PhoneNumber)
+                .Length(10)
+                .WithName(_ => propertyNames[ResourcesPropertyNames.PhoneNumber]);
+            RuleFor(resource => resource.Password)
+                .Matches(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$")
+                .WithMessage(_ => validationMessages[ResourcePropertyValidationMessages.PasswordRegex])
+                .WithName(_ => propertyNames[ResourcesPropertyNames.Password]);
+            RuleFor(resource => resource.Email)
+                .EmailAddress()
+                .WithName(_ => propertyNames[ResourcesPropertyNames.Email]);
+        }
+    }
+}
