becf961cd56f30b90b4cd1829de881b62dfd0111
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-21 09:11:26 +0330

    Add a resource record for the JWT token.

--- Source/Resource/Api/Resources/AccountResources.cs
+++ Source/Resource/Api/Resources/AccountResources.cs
@@ -2,8 +2,11 @@ using System;
 
 namespace Resource.Api.Resources
 {
+    public sealed record JwtTokenResource(string Token);
+
     public record UserRegisterResource(string PhoneNumber, string Password);
 
     public record UserLoginResource(string PhoneNumber, string Password);
+
     public record RefreshTokenResource(Guid UserId, string RefreshToken);
 }
