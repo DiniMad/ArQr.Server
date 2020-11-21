7cdb7ae1399377d73b196ccf59c0a4e626673b61
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-21 12:39:48 +0330

    Create a handler for user update me.

---
+++ Source/ArQr/Core/UserController/UserUpdateMeHandler.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ArQr.Helper;
+using AutoMapper;
+using Data.Repository.Base;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Localization;
+using Resource.Api.Resources;
+using Resource.ResourceFiles;
+
+namespace ArQr.Core.UserController
+{
+    public sealed record UserUpdateMeRequest(UserUpdateResource UpdateResource) : IRequest<ActionHandlerResult>;
+
+    public class UserUpdateMeHandler : IRequestHandler<UserUpdateMeRequest, ActionHandlerResult>
+    {
+        private readonly IHttpContextAccessor                   _httpContextAccessor;
+        private readonly IUnitOfWork                            _unitOfWork;
+        private readonly IStringLocalizer<HttpResponseMessages> _responseMessages;
+        private readonly IMapper                                _mapper;
+        private readonly IPasswordHasher<User>                  _passwordHasher;
+
+        public UserUpdateMeHandler(IHttpContextAccessor                   httpContextAccessor,
+                                   IUnitOfWork                            unitOfWork,
+                                   IStringLocalizer<HttpResponseMessages> responseMessages,
+                                   IMapper                                mapper,
+                                   IPasswordHasher<User>                  passwordHasher)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _unitOfWork          = unitOfWork;
+            _responseMessages    = responseMessages;
+            _mapper              = mapper;
+            _passwordHasher      = passwordHasher;
+        }
+
+        public async Task<ActionHandlerResult> Handle(UserUpdateMeRequest request, CancellationToken cancellationToken)
+        {
+            var (_, userId) = _httpContextAccessor.HttpContext!.GetUserAuthentication();
+            var user = await _unitOfWork.UserRepository.GetAsync(userId);
+            if (user is null)
+                return new(StatusCodes.Status404NotFound,
+                           _responseMessages[HttpResponseMessages.UserNotFound].Value);
+
+            var updateResource = request.UpdateResource;
+            user = _mapper.Map(updateResource, user);
+            if (updateResource.Password is not null)
+                user.PasswordHash = _passwordHasher.HashPassword(user, updateResource.Password);
+
+            _unitOfWork.UserRepository.Update(user);
+            await _unitOfWork.CompleteAsync();
+            
+            return new(StatusCodes.Status200OK, user);
+        }
+    }
+}
