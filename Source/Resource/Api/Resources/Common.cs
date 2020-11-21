2c858ede3d61b73b66dbd7de33054fdba189452b
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-21 08:48:25 +0330

    Create a generic record type for the API response.

---
+++ Source/Resource/Api/Resources/Common.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Resource.Api.Resources
+{
+    public sealed record ApiResponse<T>(int Status, bool Success, T? Data, object? Error) where T : class
+    {
+        public static ApiResponse<T> Parse(ObjectResult objectResult)
+        {
+            var status  = objectResult.StatusCode!.Value;
+            var success = status < 400;
+            var data    = success is true ? objectResult.Value as T : null;
+            var error   = success is false ? objectResult.Value : null;
+            return new(status, success, data, error);
+        }
+    }
+}
