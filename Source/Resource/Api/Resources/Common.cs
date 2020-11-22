836e595dd2bb69e9df1662f3936f4a14a1d2a95a
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-22 14:07:55 +0330

    Create a resource for the pagination data result.

--- Source/Resource/Api/Resources/Common.cs
+++ Source/Resource/Api/Resources/Common.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Resource.Api.Resources
@@ -19,5 +20,39 @@ namespace Resource.Api.Resources
     public sealed record PaginationInputResource(int PageSize = 5, int PageNumber = 1)
     {
         public int After => (PageNumber - 1) * PageSize;
+    }
+
+    public sealed record PaginationResultResource<TData>
+    {
+        public IEnumerable<TData> Collection { get; }
+        public int                TotalCount { get; }
+        public string?            Next       { get; }
+        public string?            Previous   { get; }
+
+        private readonly string                  _baseUrl;
+        private readonly PaginationInputResource _paginationInput;
+
+        public PaginationResultResource(IEnumerable<TData>      collection,
+                                        int                     totalCount,
+                                        string                  baseUrl,
+                                        PaginationInputResource paginationInput)
+        {
+            Collection       = collection;
+            TotalCount       = totalCount;
+            _baseUrl         = baseUrl;
+            _paginationInput = paginationInput;
+
+            var hasNext = paginationInput.After + paginationInput.PageSize < totalCount;
+            Next = hasNext ? CreateNavigationLink(1) : null;
+
+            var hasPrevious = paginationInput.After > 0;
+            Previous = hasPrevious ? CreateNavigationLink(-1) : null;
+        }
+
+        private string CreateNavigationLink(sbyte amount)
+        {
+            return
+                $"{_baseUrl}?{nameof(_paginationInput.PageNumber)}={_paginationInput.PageNumber + amount}&{nameof(_paginationInput.PageSize)}={_paginationInput.PageSize}";
+        }
     }
 }
