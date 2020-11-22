153f8bab0d57f3ce087606f7930fa7b0bbb191c2
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-22 11:04:43 +0330

    Update the GetAllUserQrCodesHandler to map qr code to qr code resource.

--- Source/ArQr/Core/QrCodeController/GetAllUserQrCodesHandler.cs
+++ Source/ArQr/Core/QrCodeController/GetAllUserQrCodesHandler.cs
@@ -1,9 +1,11 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using Data.Repository.Base;
 using MediatR;
 using Microsoft.AspNetCore.Http;
+using Resource.Api.Resources;
 
 namespace ArQr.Core.QrCodeController
 {
@@ -12,22 +14,24 @@ namespace ArQr.Core.QrCodeController
     public class GetAllUserQrCodesHandler : IRequestHandler<GetAllUserQrCodesRequest, ActionHandlerResult>
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper     _mapper;
 
-        public GetAllUserQrCodesHandler(IUnitOfWork unitOfWork)
+        public GetAllUserQrCodesHandler(IUnitOfWork unitOfWork, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
+            _mapper     = mapper;
         }
 
         public async Task<ActionHandlerResult> Handle(GetAllUserQrCodesRequest request,
                                                       CancellationToken        cancellationToken)
         {
-            var userId      = request.UserId;
-            var userQrCodes = 
+            var userId = request.UserId;
+            var userQrCodes =
                 await _unitOfWork.QrCodeRepository.FindAsync(code => code.OwnerId == userId);
-            
+
             if (userQrCodes.Any() is false) return new(StatusCodes.Status404NotFound, "NotFound");
-            
-            return new(StatusCodes.Status200OK, userQrCodes);
+
+            return new(StatusCodes.Status200OK, _mapper.Map<QrCodeResource>(userQrCodes));
         }
     }
 }
