aa9a85309e456981538b52a8b2495d1f967a080d
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-22 10:56:11 +0330

    Extract the user mapping from the MapperProfile to its own file.

--- Source/ArQr/MappingProfiles/MapperProfile.cs
+++ Source/ArQr/MappingProfiles/MapperProfile.cs
@@ -1,6 +1,5 @@
 using AutoMapper;
 using Domain;
-using Resource.Api.Resources;
 
 namespace ArQr.MappingProfiles
 {
@@ -8,19 +7,6 @@ namespace ArQr.MappingProfiles
     {
         public MapperProfile()
         {
-            CreateMap<UserRegisterResource, User>();
-            CreateMap<UserUpdateResource, User>()
-                .ForMember(user => user.EmailConfirmed,
-                           expression =>
-                               expression.MapFrom((resource, user) =>
-                                                      resource.Email is null && user.EmailConfirmed))
-                .ForMember(user => user.PhoneNumberConfirmed,
-                           expression =>
-                               expression.MapFrom((resource, user) =>
-                                                      resource.PhoneNumber is null && user.PhoneNumberConfirmed))
-                .ForAllMembers(expression =>
-                                   expression.Condition((_, _, sourceMember) => sourceMember is not null));
-            CreateMap<User, UserResource>();
             CreateMap<UserRefreshToken, UserRefreshToken>()
                 .ForMember(token => token.UserId, option => option.Ignore());
         }
---
+++ Source/ArQr/MappingProfiles/UserMappingProfile.cs
@@ -0,0 +1,29 @@
+using AutoMapper;
+using Domain;
+using Resource.Api.Resources;
+
+namespace ArQr.MappingProfiles
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<UserRegisterResource, User>();
+
+            CreateMap<UserUpdateResource, User>()
+                .ForMember(user => user.EmailConfirmed,
+                           expression =>
+                               expression.MapFrom((resource, user) =>
+                                                      resource.Email is null && user.EmailConfirmed))
+                .ForMember(user => user.PhoneNumberConfirmed,
+                           expression =>
+                               expression.MapFrom((resource, user) =>
+                                                      resource.PhoneNumber is null &&
+                                                      user.PhoneNumberConfirmed))
+                .ForAllMembers(expression =>
+                                   expression.Condition((_, _, sourceMember) => sourceMember is not null));
+
+            CreateMap<User, UserResource>();
+        }
+    }
+}
