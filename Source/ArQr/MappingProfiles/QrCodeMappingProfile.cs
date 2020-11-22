6afeabb0144ba2e87d7cfcec502d4e3822d9b92b
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-22 11:01:49 +0330

    Create a mapper for mapping qr code to qr code resource.

---
+++ Source/ArQr/MappingProfiles/QrCodeMappingProfile.cs
@@ -0,0 +1,19 @@
+using System;
+using AutoMapper;
+using Domain;
+using Resource.Api.Resources;
+
+namespace ArQr.MappingProfiles
+{
+    public class QrCodeMappingProfile : Profile
+    {
+        public QrCodeMappingProfile()
+        {
+            CreateMap<QrCode, QrCodeResource>()
+                .ForMember(resource => resource.Expired,
+                           expression =>
+                               expression.MapFrom(code =>
+                                                      code.ExpireDate > DateTimeOffset.UtcNow.ToUnixTimeSeconds()));
+        }
+    }
+}
