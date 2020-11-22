dc3369a81fa3ac8744caa6ca460715767a695848
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-22 12:43:00 +0330

    Add an overload into the qr code repository for pagination query.

--- Source/Data/Repository/Base/IQrCodeRepository.cs
+++ Source/Data/Repository/Base/IQrCodeRepository.cs
@@ -1,8 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 using Domain;
 
 namespace Data.Repository.Base
 {
     public interface IQrCodeRepository : IRepository<QrCode>
     {
+        Task<IEnumerable<QrCode>> FindAsync(Expression<Func<QrCode, bool>> predicate,int after,int take);
     }
 }
--- Source/Data/Repository/QrCodeRepository.cs
+++ Source/Data/Repository/QrCodeRepository.cs
@@ -1,13 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 using Data.Repository.Base;
 using Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Data.Repository
 {
-    public class QrCodeRepository: Repository<QrCode>,IQrCodeRepository
+    public class QrCodeRepository : Repository<QrCode>, IQrCodeRepository
     {
         public QrCodeRepository(DbContext context) : base(context)
         {
+        }
+
+        public async Task<IEnumerable<QrCode>> FindAsync(Expression<Func<QrCode, bool>> predicate, int after, int take)
+        {
+            return await Context.Set<QrCode>().Where(predicate).Skip(after).Take(take).ToListAsync();
         }
     }
 }
