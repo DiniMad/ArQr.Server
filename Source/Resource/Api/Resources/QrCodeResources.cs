9ed6ad321866737a14d6e83198ea5941a27d1c1b
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-22 11:52:18 +0330

    Modify the QrCodeResource properties to "init" mode.

--- Source/Resource/Api/Resources/QrCodeResources.cs
+++ Source/Resource/Api/Resources/QrCodeResources.cs
@@ -1,11 +1,14 @@
 namespace Resource.Api.Resources
 {
-    public sealed record QrCodeResource(string  Title,
-                                        string  Description,
-                                        int     CreationDate,
-                                        int     ExpireDate,
-                                        string? AssociatedPhoneNumber,
-                                        string? AssociatedWebsite,
-                                        bool    Expired,
-                                        long    OwnerId);
+    public sealed record QrCodeResource
+    {
+        public string  Title                 { get; init; }
+        public string  Description           { get; init; }
+        public int     CreationDate          { get; init; }
+        public int     ExpireDate            { get; init; }
+        public string? AssociatedPhoneNumber { get; init; }
+        public string? AssociatedWebsite     { get; init; }
+        public bool    Expired               { get; init; }
+        public long    OwnerId               { get; init; }
+    }
 }
