0cf252c0f01ad1c29f32cedb82fc9ce9b6630595
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-23 20:57:05 +0330

    Migration files for the Creating the QrCodeViewer table with its relation.

---
+++ Source/Data/Migrations/20201123172230_CreateTheQrCodeViewersTableWithOneToManyRelationFromQrCodeToQrCodeViewers.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class CreateTheQrCodeViewersTableWithOneToManyRelationFromQrCodeToQrCodeViewers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "QrCodeViewer",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false),
+                    QrCodeId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QrCodeViewer", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QrCodeViewer_QrCodes_QrCodeId",
+                        column: x => x.QrCodeId,
+                        principalTable: "QrCodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QrCodeViewer_QrCodeId",
+                table: "QrCodeViewer",
+                column: "QrCodeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QrCodeViewer");
+        }
+    }
+}
