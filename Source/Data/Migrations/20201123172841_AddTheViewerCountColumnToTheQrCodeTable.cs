c50c1ae77735fa0142337bcbb8f166bb14b75e14
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-23 20:59:47 +0330

    Add migration files for adding the ViewerCount to the QrCode table.

---
+++ Source/Data/Migrations/20201123172841_AddTheViewerCountColumnToTheQrCodeTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class AddTheViewerCountColumnToTheQrCodeTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ViewersCount",
+                table: "QrCodes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ViewersCount",
+                table: "QrCodes");
+        }
+    }
+}
