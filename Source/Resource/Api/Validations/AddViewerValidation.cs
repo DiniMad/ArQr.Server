4bbb6504d78b38087be7269f243a692b0c902e73
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-24 15:04:07 +0330

    Modify the add viewer validation to use the resource property names.

--- Source/Resource/Api/Validations/AddViewerValidation.cs
+++ Source/Resource/Api/Validations/AddViewerValidation.cs
@@ -1,13 +1,18 @@
 using FluentValidation;
+using Microsoft.Extensions.Localization;
 using Resource.Api.Resources;
+using Resource.ResourceFiles;
 
 namespace Resource.Api.Validations
 {
     public class AddViewerValidation : AbstractValidator<AddViewerResource>
     {
-        public AddViewerValidation()
+        public AddViewerValidation(IStringLocalizer<ResourcesPropertyNames> propertyNames)
         {
-            RuleFor(resource => resource.ViewerId).NotNull().GreaterThan(0);
+            RuleFor(resource => resource.ViewerId)
+                .NotNull()
+                .GreaterThan(0)
+                .WithName(_ => propertyNames[ResourcesPropertyNames.ViewerId]);
         }
     }
 }
