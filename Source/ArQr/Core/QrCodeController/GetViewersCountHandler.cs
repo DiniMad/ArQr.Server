5fa24d1ab4f15da36dad8510e3b2c5fe57f78453
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-24 15:42:37 +0330

    Modify the get viewers count handler to use an instance of the cache options.

--- Source/ArQr/Core/QrCodeController/GetViewersCountHandler.cs
+++ Source/ArQr/Core/QrCodeController/GetViewersCountHandler.cs
@@ -1,9 +1,12 @@
 using System.Threading;
 using System.Threading.Tasks;
+using ArQr.Helper;
 using ArQr.Interface;
+using ArQr.Models;
 using Data.Repository.Base;
 using MediatR;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace ArQr.Core.QrCodeController
 {
@@ -11,35 +14,37 @@ namespace ArQr.Core.QrCodeController
 
     public class GetViewersCountHandler : IRequestHandler<GetViewersCountRequest, ActionHandlerResult>
     {
-        private const string GhostPrefix                 = "gh";
-        private const string QrCodePrefix                = "qc";
-        private const string PersistedViewersCountPrefix = "pvc";
-        private const string ViewersListPrefix           = "vl";
-
         private readonly ICacheService _cacheService;
         private readonly IUnitOfWork   _unitOfWork;
+        private readonly CacheOptions  _cacheOptions;
 
-        public GetViewersCountHandler(ICacheService cacheService, IUnitOfWork unitOfWork)
+        public GetViewersCountHandler(ICacheService  cacheService,
+                                      IUnitOfWork    unitOfWork,
+                                      IConfiguration configuration)
         {
             _cacheService = cacheService;
             _unitOfWork   = unitOfWork;
+            _cacheOptions = configuration.GetCacheOptions();
         }
 
         public async Task<ActionHandlerResult> Handle(GetViewersCountRequest request,
                                                       CancellationToken      cancellationToken)
         {
-            var qrCodeId      = request.QrCodeId;
-            var ghostKey      = RedisSequenceKeyBuilder(GhostPrefix, QrCodePrefix, qrCodeId);
+            var qrCodeId = request.QrCodeId;
+            var ghostKey = RedisSequenceKeyBuilder(_cacheOptions.GhostPrefix, _cacheOptions.QrCodePrefix, qrCodeId);
             var ghostKeyExist = await _cacheService.KeyExist(ghostKey);
             if (ghostKeyExist is true)
             {
                 var qrCodePersistedViewersCountKey =
-                    RedisSequenceKeyBuilder(QrCodePrefix, PersistedViewersCountPrefix, qrCodeId);
+                    RedisSequenceKeyBuilder(_cacheOptions.QrCodePrefix,
+                                            _cacheOptions.PersistedViewersCountPrefix,
+                                            qrCodeId);
                 var persistedViewersCount = await _cacheService.GetAsync(qrCodePersistedViewersCountKey);
                 if (persistedViewersCount is null) return new(StatusCodes.Status500InternalServerError, "Exception");
 
-                var cachedViewerListKey = RedisSequenceKeyBuilder(QrCodePrefix, ViewersListPrefix, qrCodeId);
-                var cachedViewersCount  = await _cacheService.GetCountOfListAsync(cachedViewerListKey);
+                var cachedViewerListKey =
+                    RedisSequenceKeyBuilder(_cacheOptions.QrCodePrefix, _cacheOptions.ViewersListPrefix, qrCodeId);
+                var cachedViewersCount = await _cacheService.GetCountOfListAsync(cachedViewerListKey);
 
                 var totalCount = cachedViewersCount + long.Parse(persistedViewersCount);
                 return new(StatusCodes.Status200OK, new {Count = totalCount});
