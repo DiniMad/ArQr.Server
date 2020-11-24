7e5eeb3b49563869a6ddf109e2e6ae8e0fbaaa09
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-24 18:29:51 +0330

    Complete the cache expire notification handler to handle qr code expiration.

--- Source/ArQr/Core/CacheExpireNotificationHandler.cs
+++ Source/ArQr/Core/CacheExpireNotificationHandler.cs
@@ -1,19 +1,38 @@
-using System;
 using System.Threading;
 using System.Threading.Tasks;
+using ArQr.Core.QrCodeHandlers;
+using ArQr.Helper;
+using ArQr.Models;
 using MediatR;
+using Microsoft.Extensions.Configuration;
 
 namespace ArQr.Core
 {
-    public record CacheExpireNotification(string CacheKey) : INotification
-    {
-    }
+    public sealed record CacheExpireNotification(string CacheKey) : INotification;
 
     public class CacheExpireNotificationHandler : INotificationHandler<CacheExpireNotification>
     {
+        private readonly ISender      _sender;
+        private readonly CacheOptions _cacheOptions;
+
+        public CacheExpireNotificationHandler(IConfiguration configuration, ISender sender)
+        {
+            _sender       = sender;
+            _cacheOptions = configuration.GetCacheOptions();
+        }
+
         public async Task Handle(CacheExpireNotification notification, CancellationToken cancellationToken)
         {
-            Console.WriteLine(notification.CacheKey);
+            var key        = notification.CacheKey;
+            var isGhostKey = _cacheOptions.IsGhostKey(key);
+            if (isGhostKey is false) return;
+            var rawKey   = _cacheOptions.ExtractRawKey(key);
+            var isQrCode = _cacheOptions.KeyHasPrefix(rawKey, _cacheOptions.QrCodePrefix);
+            if (isQrCode is true)
+            {
+                var qrCodeId = rawKey.Split(_cacheOptions.KyeSeparatorCharacter)[^1];
+                await _sender.Send(new ViewersCacheExpireRequest(long.Parse(qrCodeId)), cancellationToken);
+            }
         }
     }
 }
