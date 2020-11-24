0d237fece4616b6e2418b13c43e65775b87a4b55
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-24 15:08:15 +0330

    Modify the page input validation to use the resource property names.

--- Source/Resource/Api/Validations/PaginationInputValidation.cs
+++ Source/Resource/Api/Validations/PaginationInputValidation.cs
@@ -1,14 +1,20 @@
 using FluentValidation;
+using Microsoft.Extensions.Localization;
 using Resource.Api.Resources;
+using Resource.ResourceFiles;
 
 namespace Resource.Api.Validations
 {
     public class PaginationInputValidation : AbstractValidator<PaginationInputResource>
     {
-        public PaginationInputValidation()
+        public PaginationInputValidation(IStringLocalizer<ResourcesPropertyNames> propertyNames)
         {
-            RuleFor(input => input.PageSize).InclusiveBetween(1,25);
-            RuleFor(input => input.PageNumber).GreaterThan(0);
+            RuleFor(input => input.PageSize)
+                .InclusiveBetween(1, 25)
+                .WithName(_ => propertyNames[ResourcesPropertyNames.PageSize]);
+            RuleFor(input => input.PageNumber)
+                .GreaterThan(0)
+                .WithName(_ => propertyNames[ResourcesPropertyNames.PageNumber]);
         }
     }
 }
