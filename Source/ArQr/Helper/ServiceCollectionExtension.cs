693997857da6e9d06ed8f1e1f3d955c27a74cc5b
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-24 12:44:18 +0330

    Create an extension method for registering the cache service to the ArQr project DI.

--- Source/ArQr/Helper/ServiceCollectionExtension.cs
+++ Source/ArQr/Helper/ServiceCollectionExtension.cs
@@ -1,4 +1,6 @@
 using System.IdentityModel.Tokens.Jwt;
+using ArQr.Infrastructure;
+using ArQr.Interface;
 using ArQr.Models;
 using Data.Repository;
 using Data.Repository.Base;
@@ -6,6 +8,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using StackExchange.Redis;
 
 namespace ArQr.Helper
 {
@@ -46,8 +49,17 @@ namespace ArQr.Helper
                 options.AddSupportedCultures(supportedCultures);
                 options.AddSupportedUICultures(supportedCultures);
             });
-            
+
             services.AddLocalization();
+
+            return services;
+        }
+
+        public static IServiceCollection AddCacheService(this IServiceCollection services, string connectionString)
+        {
+            services.AddSingleton<IConnectionMultiplexer, ConnectionMultiplexer>(_ =>
+                ConnectionMultiplexer.Connect(connectionString));
+            services.AddSingleton<ICacheService, CacheService>();
 
             return services;
         }
