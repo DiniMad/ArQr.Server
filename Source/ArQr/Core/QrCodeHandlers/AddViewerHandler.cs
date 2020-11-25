222852f7ef05ba2079677e5d1f08922cc50ed3b9
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-25 17:38:40 +0330

    Modify the add viewer handler to not create a key-value pair for the persisted viewers count.

--- Source/ArQr/Core/QrCodeHandlers/AddViewerHandler.cs
+++ Source/ArQr/Core/QrCodeHandlers/AddViewerHandler.cs
@@ -37,25 +37,16 @@ namespace ArQr.Core.QrCodeHandlers
 
         public async Task<ActionHandlerResult> Handle(AddViewerRequest request, CancellationToken cancellationToken)
         {
-            var (ghostPrefix, qrCodePrefix, persistedViewersCountPrefix, viewersListPrefix, expireTimeInMinute) =
+            var (ghostPrefix, qrCodePrefix, _, viewersListPrefix, expireTimeInMinute) =
                 _cacheOptions;
 
-            var qrCodeId      = request.QrCodeId;
-            var ghostKey      = _cacheOptions.SequenceKeyBuilder(ghostPrefix, qrCodePrefix, qrCodeId);
-            var ghostKeyExist = await _cacheService.KeyExistAsync(ghostKey);
-            if (ghostKeyExist is false)
-            {
-                var qrCode = await _unitOfWork.QrCodeRepository.GetAsync(qrCodeId);
-                if (qrCode is null)
-                    return new(StatusCodes.Status404NotFound,
-                               _responseMessages[HttpResponseMessages.QrCodeNotFound].Value);
+            var qrCodeId = request.QrCodeId;
+            var qrCode   = await _unitOfWork.QrCodeRepository.GetAsync(qrCodeId);
+            if (qrCode is null)
+                return new(StatusCodes.Status404NotFound,
+                           _responseMessages[HttpResponseMessages.QrCodeNotFound].Value);
 
-                var qrCodePersistedViewersCountValue = qrCode.ViewersCount.ToString();
-                var qrCodePersistedViewersCountKey =
-                    _cacheOptions.SequenceKeyBuilder(qrCodePrefix, persistedViewersCountPrefix, qrCodeId);
-                await _cacheService.SetAsync(qrCodePersistedViewersCountKey, qrCodePersistedViewersCountValue);
-            }
-
+            var ghostKey = _cacheOptions.SequenceKeyBuilder(ghostPrefix, qrCodePrefix, qrCodeId);
             await _cacheService.SetAsync(ghostKey, string.Empty, TimeSpan.FromMinutes(expireTimeInMinute));
 
             var viewerListKey = _cacheOptions.SequenceKeyBuilder(qrCodePrefix, viewersListPrefix, qrCodeId);
