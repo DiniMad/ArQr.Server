9d57a5eef740b1db3b30d8ef414508864fe938b9
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-25 15:27:30 +0330

    Separate qr code resource and authorize qr code resource to two records.

--- Source/Resource/Api/Resources/QrCodeResources.cs
+++ Source/Resource/Api/Resources/QrCodeResources.cs
@@ -1,17 +1,23 @@
 namespace Resource.Api.Resources
 {
-    public sealed record QrCodeResource
+    public record QrCodeResource
     {
         public long    Id                    { get; set; }
         public string  Title                 { get; init; }
         public string  Description           { get; init; }
-        public int     CreationDate          { get; init; }
-        public int     ExpireDate            { get; init; }
         public string? AssociatedPhoneNumber { get; init; }
         public string? AssociatedWebsite     { get; init; }
-        public bool    Expired               { get; init; }
-        public long    OwnerId               { get; init; }
     }
+
+    public sealed record AuthorizeQrCodeResource : QrCodeResource
+    {
+        public int  CreationDate { get; init; }
+        public int  ExpireDate   { get; init; }
+        public bool Expired      { get; init; }
+        public long OwnerId      { get; init; }
+        public int  ViewersCount { get; set; }
+    }
+
 
     public sealed record AddViewerResource(long ViewerId);
 
