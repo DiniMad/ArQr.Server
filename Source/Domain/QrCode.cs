60c88f28abee84d95c2209ffdad5e78862ccc59b
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-25 21:22:42 +0330

    Add the "MaxAllowedViewersCount" property to the qr code domain.

--- Source/Domain/QrCode.cs
+++ Source/Domain/QrCode.cs
@@ -6,16 +6,17 @@ namespace Domain
 {
     public class QrCode : BaseDomain
     {
-        public string                    Title                 { get; set; }
-        public string                    Description           { get; set; }
-        public int                       CreationDate          { get; set; }
-        public int                       ExpireDate            { get; set; }
-        public string?                   AssociatedPhoneNumber { get; set; }
-        public string?                   AssociatedWebsite     { get; set; }
-        public int                       ViewersCount          { get; set; }
-        public User                      Owner                 { get; set; }
-        public long                      OwnerId               { get; set; }
-        public IEnumerable<QrCodeViewer> Viewers               { get; set; }
+        public string                    Title                  { get; set; }
+        public string                    Description            { get; set; }
+        public int                       CreationDate           { get; set; }
+        public int                       ExpireDate             { get; set; }
+        public string?                   AssociatedPhoneNumber  { get; set; }
+        public string?                   AssociatedWebsite      { get; set; }
+        public int                       ViewersCount           { get; set; }
+        public int                       MaxAllowedViewersCount { get; set; }
+        public User                      Owner                  { get; set; }
+        public long                      OwnerId                { get; set; }
+        public IEnumerable<QrCodeViewer> Viewers                { get; set; }
 
         public QrCode()
         {
