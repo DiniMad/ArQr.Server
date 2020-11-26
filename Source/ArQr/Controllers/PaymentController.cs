f3862471baf422bee10752a4fe3713a2ad6207e1
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-26 20:33:20 +0330

    Create the PaymentController with two actions for create invoice and verify payment.

---
+++ Source/ArQr/Controllers/PaymentController.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using ArQr.Core.PaymentHandlers;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Resource.Api.Resources;
+
+namespace ArQr.Controllers
+{
+    [Route("Payment")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PaymentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> RequestInvoice(InvoiceResource invoiceResource)
+        {
+            var (statusCode, value) = await _mediator.Send(new BuildInvoiceRequest(invoiceResource));
+            return StatusCode(statusCode, value);
+        }
+
+        public async Task<ActionResult> Verify()
+        {
+            return Ok();
+        }
+    }
+}
