f19e98278d230494e850da30025242c037d50886
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-26 18:16:11 +0330

    Create the Purchse domain.

---
+++ Source/Domain/Purchase.cs
@@ -0,0 +1,18 @@
+using Domain.Base;
+
+namespace Domain
+{
+    public class Purchase : BaseDomain<long>
+    {
+        public string   TransactionCode           { get; set; }
+        public string   GetwayName                { get; set; }
+        public byte     Quantity                  { get; set; }
+        public int      Date                      { get; set; }
+        public int      PaidAmountInThousandToman { get; set; }
+        public byte     OffAmountInThousandToman  { get; set; }
+        public long     UserId                    { get; set; }
+        public User?    User                      { get; set; }
+        public byte     ServiceId                 { get; set; }
+        public Service? Service                   { get; set; }
+    }
+}
