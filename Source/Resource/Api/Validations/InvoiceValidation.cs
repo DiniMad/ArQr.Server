42901613e283721a58886e5db432320836859f20
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-26 21:09:11 +0330

    Update the invoice validation to use resource property names.

--- Source/Resource/Api/Validations/InvoiceValidation.cs
+++ Source/Resource/Api/Validations/InvoiceValidation.cs
@@ -1,14 +1,22 @@
 using FluentValidation;
+using Microsoft.Extensions.Localization;
 using Resource.Api.Resources;
+using Resource.ResourceFiles;
 
 namespace Resource.Api.Validations
 {
     public class InvoiceValidation : AbstractValidator<InvoiceResource>
     {
-        public InvoiceValidation()
+        public InvoiceValidation(IStringLocalizer<ResourcesPropertyNames> propertyNames)
         {
-            RuleFor(resource => resource.Service).NotNull().GreaterThan((byte) 0);
-            RuleFor(resource => resource.Quantity).NotNull().GreaterThan((byte) 0);
+            RuleFor(resource => resource.Service)
+                .NotNull()
+                .GreaterThan((byte) 0)
+                .WithName(_ => propertyNames[ResourcesPropertyNames.Service]);
+            RuleFor(resource => resource.Quantity)
+                .NotNull()
+                .GreaterThan((byte) 0)
+                .WithName(_ => propertyNames[ResourcesPropertyNames.Quantity]);
         }
     }
 }
