ec631e91deb98dc083f967ebce7c543bc11ffc32
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-26 21:37:21 +0330

    Add migration files for renaming one of the Purchase table columns.

---
+++ Source/Data/Migrations/20201126180617_RenameTheGetwayNameToGatewayName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class RenameTheGetwayNameToGatewayName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GetwayName",
+                table: "Purchases",
+                newName: "GatewayName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GatewayName",
+                table: "Purchases",
+                newName: "GetwayName");
+        }
+    }
+}
