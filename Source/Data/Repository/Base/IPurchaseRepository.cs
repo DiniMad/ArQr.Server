4bae339872cb6f538328a8f7d35f018b9a54e13f
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-26 22:25:45 +0330

    Make all derived update and remove methods obsolete in the PurchaseRepository.

--- Source/Data/Repository/Base/IPurchaseRepository.cs
+++ Source/Data/Repository/Base/IPurchaseRepository.cs
@@ -1,8 +1,21 @@
+using System;
+using System.Collections.Generic;
 using Domain;
 
 namespace Data.Repository.Base
 {
     public interface IPurchaseRepository : IRepository<Purchase, long>
     {
+        [Obsolete("This repository should not have update method.", true)]
+        void Update(Purchase domain);
+
+        [Obsolete("This repository should not have update method.", true)]
+        void UpdateCollection(IEnumerable<Purchase> domains);
+
+        [Obsolete("This repository should not have remove method.", true)]
+        void Remove(Purchase domain);
+
+        [Obsolete("This repository should not have remove method.", true)]
+        void RemoveCollection(IEnumerable<Purchase> domains);
     }
 }
