c02cc7b73cd3fbd87ca7003fa52b2b91c14e1914
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-26 18:17:21 +0330

    Add migration files for the creating the Purchase table.

---
+++ Source/Data/Migrations/20201126144433_CreateThePurchaseTableWithItsRelashion.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class CreateThePurchaseTableWithItsRelashion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Purchases",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TransactionCode = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    GetwayName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Quantity = table.Column<byte>(type: "tinyint", nullable: false),
+                    Date = table.Column<int>(type: "int", nullable: false),
+                    PaidAmountInThousandToman = table.Column<int>(type: "int", nullable: false),
+                    OffAmountInThousandToman = table.Column<byte>(type: "tinyint", nullable: false),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    ServiceId = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Purchases", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Purchases_Services_ServiceId",
+                        column: x => x.ServiceId,
+                        principalTable: "Services",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Purchases_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Purchases_ServiceId",
+                table: "Purchases",
+                column: "ServiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Purchases_TransactionCode",
+                table: "Purchases",
+                column: "TransactionCode",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Purchases_UserId",
+                table: "Purchases",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Purchases");
+        }
+    }
+}
