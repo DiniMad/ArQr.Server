73307470464335c0364ad9388e10264f1f5b607d
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-26 18:40:33 +0330

    Config the Id column of the Service table to generate value automatically.

--- Source/Data/Configuration/ServiceConfiguration.cs
+++ Source/Data/Configuration/ServiceConfiguration.cs
@@ -8,6 +8,7 @@ namespace Data.Configuration
     {
         public void Configure(EntityTypeBuilder<Service> builder)
         {
+            builder.Property(service => service.Id).ValueGeneratedOnAdd();
             builder.Property(service => service.Title).HasMaxLength(32);
             builder.Property(service => service.Description).HasMaxLength(128);
         }
