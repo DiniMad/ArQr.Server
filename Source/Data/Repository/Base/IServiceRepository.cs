5a8a59e3b59209f816dfb89de0811e6ed3c201f6
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-26 18:59:23 +0330

    Make all derived update and remove methods obsolete in the ServiceRepository.

--- Source/Data/Repository/Base/IServiceRepository.cs
+++ Source/Data/Repository/Base/IServiceRepository.cs
@@ -1,8 +1,21 @@
+using System;
+using System.Collections.Generic;
 using Domain;
 
 namespace Data.Repository.Base
 {
     public interface IServiceRepository : IRepository<Service, byte>
     {
+        [Obsolete("This repository should not have update method.", true)]
+        void Update(Service domain);
+
+        [Obsolete("This repository should not have update method.", true)]
+        void UpdateCollection(IEnumerable<Service> domains);
+
+        [Obsolete("This repository should not have remove method.", true)]
+        void Remove(Service domain);
+
+        [Obsolete("This repository should not have remove method.", true)]
+        void RemoveCollection(IEnumerable<Service> domains);
     }
 }
