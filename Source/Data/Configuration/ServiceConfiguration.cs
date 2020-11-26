81b066ccee8fca67ba9acb5f7f19b277fad00018
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-26 17:56:57 +0330

    Create a configuration file for the Service domain.

---
+++ Source/Data/Configuration/ServiceConfiguration.cs
@@ -0,0 +1,15 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.Configuration
+{
+    public class ServiceConfiguration : IEntityTypeConfiguration<Service>
+    {
+        public void Configure(EntityTypeBuilder<Service> builder)
+        {
+            builder.Property(service => service.Title).HasMaxLength(32);
+            builder.Property(service => service.Description).HasMaxLength(128);
+        }
+    }
+}
