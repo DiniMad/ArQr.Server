29c50d387085af4475113f96436870024804b1ee
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-27 15:17:07 +0330

    Add migration files for creating navigation column in the media content table.

---
+++ Source/Data/Migrations/20201127114411_CreateNaviagtionPropertyToSupportedMediaExtensionTableInMediaContentTable.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class CreateNaviagtionPropertyToSupportedMediaExtensionTableInMediaContentTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Extension",
+                table: "MediaContents");
+
+            migrationBuilder.AddColumn<byte>(
+                name: "ExtensionId",
+                table: "MediaContents",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MediaContents_ExtensionId",
+                table: "MediaContents",
+                column: "ExtensionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MediaContents_SupportedMediaExtensions_ExtensionId",
+                table: "MediaContents",
+                column: "ExtensionId",
+                principalTable: "SupportedMediaExtensions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MediaContents_SupportedMediaExtensions_ExtensionId",
+                table: "MediaContents");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MediaContents_ExtensionId",
+                table: "MediaContents");
+
+            migrationBuilder.DropColumn(
+                name: "ExtensionId",
+                table: "MediaContents");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Extension",
+                table: "MediaContents",
+                type: "varchar(8)",
+                unicode: false,
+                maxLength: 8,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
