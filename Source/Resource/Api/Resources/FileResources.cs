f3bd2637c16fdb3a36b1532bf7acf74da9d9ee59
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-27 17:04:54 +0330

    Create a resource file for upload session.

--- Source/Resource/Api/Resources/FileResources.cs
+++ Source/Resource/Api/Resources/FileResources.cs
@@ -1,4 +1,8 @@
+using System;
+
 namespace Resource.Api.Resources
 {
     public sealed record CreateUploadSessionResource(long MediaContentId, string Extension, byte TotalSizeInMb);
+
+    public sealed record UploadSessionResource(Guid Session);
 }
