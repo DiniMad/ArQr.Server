3797e115d4e27b7a11ab5b050897f6d090d28ec2
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-27 15:20:20 +0330

    Add a property of type of the supported media extension repository to the unit of work.

--- Source/Data/Repository/Base/IUnitOfWork.cs
+++ Source/Data/Repository/Base/IUnitOfWork.cs
@@ -5,12 +5,13 @@ namespace Data.Repository.Base
 {
     public interface IUnitOfWork : IAsyncDisposable
     {
-        public IUserRepository          UserRepository          { get; }
-        public IQrCodeRepository        QrCodeRepository        { get; }
-        public IQrCodeViewersRepository QrCodeViewersRepository { get; }
-        public IServiceRepository       ServiceRepository       { get; }
-        public IPurchaseRepository      PurchaseRepository      { get; }
-        public IMediaContentRepository  MediaContentRepository  { get; }
+        public IUserRepository                    UserRepository                    { get; }
+        public IQrCodeRepository                  QrCodeRepository                  { get; }
+        public IQrCodeViewersRepository           QrCodeViewersRepository           { get; }
+        public IServiceRepository                 ServiceRepository                 { get; }
+        public IPurchaseRepository                PurchaseRepository                { get; }
+        public IMediaContentRepository            MediaContentRepository            { get; }
+        public ISupportedMediaExtensionRepository SupportedMediaExtensionRepository { get; }
 
         public Task CompleteAsync();
     }
--- Source/Data/Repository/UnitOfWork.cs
+++ Source/Data/Repository/UnitOfWork.cs
@@ -6,25 +6,27 @@ namespace Data.Repository
 {
     public class UnitOfWork : IUnitOfWork
     {
-        private ApplicationDbContext     DbContext               { get; }
-        public  IUserRepository          UserRepository          { get; }
-        public  IQrCodeRepository        QrCodeRepository        { get; }
-        public  IQrCodeViewersRepository QrCodeViewersRepository { get; }
-        public  IServiceRepository       ServiceRepository       { get; }
-        public  IPurchaseRepository      PurchaseRepository      { get; }
-        public  IMediaContentRepository  MediaContentRepository  { get; }
+        private ApplicationDbContext               DbContext                         { get; }
+        public  IUserRepository                    UserRepository                    { get; }
+        public  IQrCodeRepository                  QrCodeRepository                  { get; }
+        public  IQrCodeViewersRepository           QrCodeViewersRepository           { get; }
+        public  IServiceRepository                 ServiceRepository                 { get; }
+        public  IPurchaseRepository                PurchaseRepository                { get; }
+        public  IMediaContentRepository            MediaContentRepository            { get; }
+        public  ISupportedMediaExtensionRepository SupportedMediaExtensionRepository { get; }
 
         public UnitOfWork()
         {
             var dbContextFactory = new DesignTimeDbContextFactory();
             DbContext = dbContextFactory.CreateDbContext(Array.Empty<string>());
 
-            UserRepository          = new UserRepository(DbContext);
-            QrCodeRepository        = new QrCodeRepository(DbContext);
-            QrCodeViewersRepository = new QrCodeViewersRepository(DbContext);
-            ServiceRepository       = new ServiceRepository(DbContext);
-            PurchaseRepository      = new PurchaseRepository(DbContext);
-            MediaContentRepository  = new MediaContentRepository(DbContext);
+            UserRepository                    = new UserRepository(DbContext);
+            QrCodeRepository                  = new QrCodeRepository(DbContext);
+            QrCodeViewersRepository           = new QrCodeViewersRepository(DbContext);
+            ServiceRepository                 = new ServiceRepository(DbContext);
+            PurchaseRepository                = new PurchaseRepository(DbContext);
+            MediaContentRepository            = new MediaContentRepository(DbContext);
+            SupportedMediaExtensionRepository = new SupportedMediaExtensionRepository(DbContext);
         }
 
         public async Task CompleteAsync()
