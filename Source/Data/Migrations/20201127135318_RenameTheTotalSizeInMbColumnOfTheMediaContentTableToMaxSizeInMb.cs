9c96e5e3fd97ea4bb8467cdfc805f8f3ab7bf669
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-27 17:24:45 +0330

    Add migration files for renaming the media content table column.

---
+++ Source/Data/Migrations/20201127135318_RenameTheTotalSizeInMbColumnOfTheMediaContentTableToMaxSizeInMb.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class RenameTheTotalSizeInMbColumnOfTheMediaContentTableToMaxSizeInMb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TotalSizeInMb",
+                table: "MediaContents",
+                newName: "MaxSizeInMb");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MaxSizeInMb",
+                table: "MediaContents",
+                newName: "TotalSizeInMb");
+        }
+    }
+}
