9b7981efc935acb5cef8111909a01feeb4a57aec
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-27 15:07:31 +0330

    Add DbSet property for the supported media extesnion and its configuration to the application db context.

--- Source/Data/ApplicationDbContext.cs
+++ Source/Data/ApplicationDbContext.cs
@@ -6,12 +6,13 @@ namespace Data
 {
     internal class ApplicationDbContext : DbContext
     {
-        public DbSet<User>         Users         { get; set; }
-        public DbSet<QrCode>       QrCodes       { get; set; }
-        public DbSet<QrCodeViewer> QrCodeViewers { get; set; }
-        public DbSet<Service>      Services      { get; set; }
-        public DbSet<Purchase>     Purchases     { get; set; }
-        public DbSet<MediaContent> MediaContents { get; set; }
+        public DbSet<User>                    Users                    { get; set; }
+        public DbSet<QrCode>                  QrCodes                  { get; set; }
+        public DbSet<QrCodeViewer>            QrCodeViewers            { get; set; }
+        public DbSet<Service>                 Services                 { get; set; }
+        public DbSet<Purchase>                Purchases                { get; set; }
+        public DbSet<MediaContent>            MediaContents            { get; set; }
+        public DbSet<SupportedMediaExtension> SupportedMediaExtensions { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -26,6 +27,7 @@ namespace Data
             modelBuilder.ApplyConfiguration(new ServiceConfiguration());
             modelBuilder.ApplyConfiguration(new PurchaseConfiguration());
             modelBuilder.ApplyConfiguration(new MediaContentConfiguration());
+            modelBuilder.ApplyConfiguration(new SupportedMediaExtensionConfiguration());
         }
     }
 }
