b9f1366b8783a396a9290aa06ccc30dc999b20f4
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-27 23:23:40 +0330

    Modify the file storage to use the static "Directory" class.

--- Source/ArQr/Infrastructure/FileStorage.cs
+++ Source/ArQr/Infrastructure/FileStorage.cs
@@ -8,21 +8,19 @@ namespace ArQr.Infrastructure
 {
     public class FileStorage : IFileStorage
     {
-        public           string        BasePath { get; }
-        private readonly DirectoryInfo _directoryInfo;
+        public string BasePath { get; }
 
         public FileStorage(IConfiguration configuration)
         {
-            BasePath       = configuration.GetFileStoragePath();
-            _directoryInfo = new DirectoryInfo(BasePath);
-            if (_directoryInfo.Exists is false) _directoryInfo.Create();
+            BasePath = configuration.GetFileStoragePath();
+            var baseDirectoryExist = Directory.Exists(BasePath);
+            if (baseDirectoryExist is false) Directory.CreateDirectory(BasePath);
         }
 
         public async Task WriteFileAsync(string path, string fileName, byte[] content)
         {
             var filePath = Path.Join(BasePath, path, fileName);
 
-            _directoryInfo.CreateSubdirectory(path);
             await using var file = File.OpenWrite(filePath);
             await file.WriteAsync(content);
         }
