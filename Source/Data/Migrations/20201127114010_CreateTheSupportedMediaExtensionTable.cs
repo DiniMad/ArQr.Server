bf4a5c9fbc6e0eea93391c10658b916d19934f48
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-27 15:11:04 +0330

    Add migration files for creating the supported media extension table.

---
+++ Source/Data/Migrations/20201127114010_CreateTheSupportedMediaExtensionTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class CreateTheSupportedMediaExtensionTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SupportedMediaExtensions",
+                columns: table => new
+                {
+                    Id = table.Column<byte>(type: "tinyint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Extension = table.Column<string>(type: "varchar(8)", unicode: false, maxLength: 8, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SupportedMediaExtensions", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SupportedMediaExtensions_Extension",
+                table: "SupportedMediaExtensions",
+                column: "Extension",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SupportedMediaExtensions");
+        }
+    }
+}
