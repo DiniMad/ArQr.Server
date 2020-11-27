f1976a12996a48a95b6265d6df57589feac7ea90
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-27 14:30:12 +0330

    Add a property of collection of the content media to the user domain.

--- Source/Domain/User.cs
+++ Source/Domain/User.cs
@@ -5,13 +5,14 @@ namespace Domain
 {
     public class User : BaseDomain<long>
     {
-        public string                PhoneNumber          { get; set; }
-        public string                PasswordHash         { get; set; }
-        public string                Email                { get; set; }
-        public bool                  EmailConfirmed       { get; set; }
-        public bool                  PhoneNumberConfirmed { get; set; }
-        public UserRefreshToken      RefreshToken         { get; set; }
-        public IEnumerable<QrCode>   QrCodes              { get; set; }
-        public IEnumerable<Purchase> Purchases            { get; set; }
+        public string                    PhoneNumber          { get; set; }
+        public string                    PasswordHash         { get; set; }
+        public string                    Email                { get; set; }
+        public bool                      EmailConfirmed       { get; set; }
+        public bool                      PhoneNumberConfirmed { get; set; }
+        public UserRefreshToken          RefreshToken         { get; set; }
+        public IEnumerable<QrCode>       QrCodes              { get; set; }
+        public IEnumerable<Purchase>     Purchases            { get; set; }
+        public IEnumerable<MediaContent> MediaContents        { get; set; }
     }
 }
