b219656083a99e8e29bbf11418e5b914cbfcd190
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-27 14:30:57 +0330

    Add migration files for the creating the media content table.

---
+++ Source/Data/Migrations/20201127105759_CreateMediaContentTableWithItsRelation.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class CreateMediaContentTableWithItsRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MediaContents",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Extension = table.Column<string>(type: "varchar(8)", unicode: false, maxLength: 8, nullable: false),
+                    Verified = table.Column<bool>(type: "bit", nullable: false),
+                    TotalSizeInMb = table.Column<byte>(type: "tinyint", nullable: false),
+                    CreationDate = table.Column<int>(type: "int", nullable: false),
+                    ExpireDate = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MediaContents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MediaContents_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MediaContents_UserId",
+                table: "MediaContents",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MediaContents");
+        }
+    }
+}
