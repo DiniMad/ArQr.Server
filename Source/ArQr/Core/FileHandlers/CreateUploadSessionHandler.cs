88bfd3cf1a8eef267b58f1b11abd53527b7fe237
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-27 17:34:55 +0330

    Modify the create upload session handler to use the HTTP response messages.

--- Source/ArQr/Core/FileHandlers/CreateUploadSessionHandler.cs
+++ Source/ArQr/Core/FileHandlers/CreateUploadSessionHandler.cs
@@ -3,7 +3,6 @@ using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using ArQr.Helper;
-using ArQr.Infrastructure;
 using ArQr.Interface;
 using ArQr.Models;
 using Data.Repository.Base;
@@ -53,7 +52,9 @@ namespace ArQr.Core.FileHandlers
                            _responseMessages[HttpResponseMessages.MediaContentNotFound]);
 
             if (totalSizeInMb > mediaContent.MaxSizeInMb)
-                return new(StatusCodes.Status400BadRequest, "ViolationOfMediaMaxSize");
+                return new(StatusCodes.Status400BadRequest,
+                           _responseMessages[HttpResponseMessages.ViolationOfMediaMaxSize]);
+
 
             var extension = await _unitOfWork.SupportedMediaExtensionRepository.GetAsync(extensionName);
             if (extension is null)
