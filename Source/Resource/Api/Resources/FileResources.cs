51016fa9e4f925b8d77ad75d44f805702fa63e0b
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-27 19:35:30 +0330

    Create a resource file for upload chunks.

--- Source/Resource/Api/Resources/FileResources.cs
+++ Source/Resource/Api/Resources/FileResources.cs
@@ -5,4 +5,6 @@ namespace Resource.Api.Resources
     public sealed record CreateUploadSessionResource(long MediaContentId, string Extension, byte TotalSizeInMb);
 
     public sealed record UploadSessionResource(Guid Session);
+
+    public sealed record UploadChunkResource(Guid Session, byte ChunkNumber, byte[] Content);
 }
