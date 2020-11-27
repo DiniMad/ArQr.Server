d1581086bf48654c9cc60bb02894be5ad6cca288
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-27 16:58:18 +0330

    Create the FileController with an action for creating upload session.

---
+++ Source/ArQr/Controllers/FileController.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using ArQr.Core.FileHandlers;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Resource.Api.Resources;
+
+namespace ArQr.Controllers
+{
+    [ApiController]
+    [Route("File")]
+    public class FileController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FileController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [Authorize]
+        [HttpPost("createSession")]
+        public async Task<ActionResult> CreateUploadSession(CreateUploadSessionResource sessionResource)
+        {
+            var (statusCode, value) = await _mediator.Send(new CreateUploadSessionRequest(sessionResource));
+            return StatusCode(statusCode, value);
+        }
+    }
+}
