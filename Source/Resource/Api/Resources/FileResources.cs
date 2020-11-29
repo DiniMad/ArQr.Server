4e50c9a38a90bf3013e7d5592488d665c66923c9
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-29 18:11:34 +0330

    Create a resource record for upload completed.

--- Source/Resource/Api/Resources/FileResources.cs
+++ Source/Resource/Api/Resources/FileResources.cs
@@ -7,4 +7,6 @@ namespace Resource.Api.Resources
     public sealed record UploadSessionResource(Guid Session);
 
     public sealed record UploadChunkResource(Guid Session, byte ChunkNumber, byte[] Content);
+
+    public sealed record UploadCompletedResource(Guid Session);
 }
