fb308fff1758735f96b92cea1cc7d53138271bfb
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-29 18:32:59 +0330

    Update the cache expire notification handler to detect upload session expiration and make a upload session expired request.

--- Source/ArQr/Core/CacheExpireNotificationHandler.cs
+++ Source/ArQr/Core/CacheExpireNotificationHandler.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using System.Threading.Tasks;
+using ArQr.Core.FileHandlers;
 using ArQr.Core.QrCodeHandlers;
 using ArQr.Helper;
 using ArQr.Models;
@@ -27,7 +28,8 @@ namespace ArQr.Core
             var key        = notification.CacheKey;
             var isGhostKey = _cacheOptions.IsGhostKey(key);
             if (isGhostKey is false) return;
-            var rawKey   = _cacheOptions.ExtractRawKey(key);
+            var rawKey = _cacheOptions.ExtractRawKey(key);
+
             var isQrCode = _cacheOptions.KeyHasPrefix(rawKey, _cacheOptions.QrCodePrefix);
             if (isQrCode is true)
             {
@@ -36,6 +38,16 @@ namespace ArQr.Core
                 using var serviceScope = _scopeFactory.CreateScope();
                 var       sender       = serviceScope.ServiceProvider.GetRequiredService<ISender>();
                 await sender.Send(new ViewersCacheExpireRequest(long.Parse(qrCodeId)), cancellationToken);
+            }
+
+            var isUploadSession = _cacheOptions.KeyHasPrefix(rawKey, _cacheOptions.UploadSessionPrefix);
+            if (isUploadSession is true)
+            {
+                var session = rawKey.Split(_cacheOptions.KyeSeparatorCharacter)[^1];
+
+                using var serviceScope = _scopeFactory.CreateScope();
+                var       sender       = serviceScope.ServiceProvider.GetRequiredService<ISender>();
+                await sender.Send(new UploadSessionExpiredRequest(session), cancellationToken);
             }
         }
     }
