965ab4d7d0795ac450b9187b2f323eaf786307c5
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-29 20:11:02 +0330

    Create a helper class for response messages.

---
+++ Source/ArQr/Helper/ResponseMessages.cs
@@ -0,0 +1,36 @@
+using ArQr.Interface;
+using Microsoft.Extensions.Localization;
+using Resource.ResourceFiles;
+
+namespace ArQr.Helper
+{
+    public class ResponseMessages : IResponseMessages
+    {
+        private readonly IStringLocalizer<HttpResponseMessages> _responseMessages;
+
+        public ResponseMessages(IStringLocalizer<HttpResponseMessages> responseMessages)
+        {
+            _responseMessages = responseMessages;
+        }
+
+        public  string Unauthorized()                => GetMessage(HttpResponseMessages.Unauthorized);
+        public  string UserNotFound()                => GetMessage(HttpResponseMessages.UserNotFound);
+        public  string IncorrectPassword()           => GetMessage(HttpResponseMessages.IncorrectPassword);
+        public  string IncorrectRefreshToken()       => GetMessage(HttpResponseMessages.IncorrectRefreshToken);
+        public  string DuplicatePhoneNumber()        => GetMessage(HttpResponseMessages.DuplicatePhoneNumber);
+        public  string UnhandledException()          => GetMessage(HttpResponseMessages.UnhandledException);
+        public  string DuplicatePhoneNumberOrEmail() => GetMessage(HttpResponseMessages.DuplicatePhoneNumberOrEmail);
+        public  string QrCodeNotFound()              => GetMessage(HttpResponseMessages.QrCodeNotFound);
+        public  string Done()                        => GetMessage(HttpResponseMessages.Done);
+        public  string ServiceNotFound()             => GetMessage(HttpResponseMessages.ServiceNotFound);
+        public  string PaymentAlreadyProcessed()     => GetMessage(HttpResponseMessages.PaymentAlreadyProcessed);
+        public  string PaymentAlreadyVerified()      => GetMessage(HttpResponseMessages.PaymentAlreadyVerified);
+        public  string PaymentExpired()              => GetMessage(HttpResponseMessages.PaymentExpired);
+        public  string WrongPayment()                => GetMessage(HttpResponseMessages.WrongPayment);
+        public  string MediaContentNotFound()        => GetMessage(HttpResponseMessages.MediaContentNotFound);
+        public  string ExtensionNotSupported()       => GetMessage(HttpResponseMessages.ExtensionNotSupported);
+        public  string ViolationOfMediaMaxSize()     => GetMessage(HttpResponseMessages.ViolationOfMediaMaxSize);
+        public  string SessionExpired()              => GetMessage(HttpResponseMessages.SessionExpired);
+        private string GetMessage(string key)        => _responseMessages[key].Value;
+    }
+}
---
+++ Source/ArQr/Interface/IResponseMessages.cs
@@ -0,0 +1,24 @@
+namespace ArQr.Interface
+{
+    public interface IResponseMessages
+    {
+        public string Unauthorized();
+        public string UserNotFound();
+        public string IncorrectPassword();
+        public string IncorrectRefreshToken();
+        public string DuplicatePhoneNumber();
+        public string UnhandledException();
+        public string DuplicatePhoneNumberOrEmail();
+        public string QrCodeNotFound();
+        public string Done();
+        public string ServiceNotFound();
+        public string PaymentAlreadyProcessed();
+        public string PaymentAlreadyVerified();
+        public string PaymentExpired();
+        public string WrongPayment();
+        public string MediaContentNotFound();
+        public string ExtensionNotSupported();
+        public string ViolationOfMediaMaxSize();
+        public string SessionExpired();
+    }
+}
