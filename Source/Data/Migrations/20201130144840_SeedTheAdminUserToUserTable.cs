a91c0d76a37876c22f54785e404cb7735eb58158
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-30 18:20:07 +0330

    Add migration files for seeding the admin user to the user table.

---
+++ Source/Data/Migrations/20201130144840_SeedTheAdminUserToUserTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class SeedTheAdminUserToUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Admin", "Email", "EmailConfirmed", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed" },
+                values: new object[] { 1L, true, "admin@arqr.com", true, "AQAAAAEAACcQAAAAELOXD5Z4lKhjCEQExaoM+Z0q7BR/vISBHA1XNP6nxJI2MrCD/x6vVCqEHCQ+mOHITg==", "0000000000", true });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1L);
+        }
+    }
+}
