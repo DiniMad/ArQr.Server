0e8e67b1eaaced8c589539868366151bd6883696
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-30 22:19:47 +0330

    Make the navigation property called extension in the media content nullable.

--- Source/Domain/MediaContent.cs
+++ Source/Domain/MediaContent.cs
@@ -4,13 +4,13 @@ namespace Domain
 {
     public class MediaContent : BaseDomain<long>
     {
-        public bool                    Verified     { get; set; }
-        public byte                    MaxSizeInMb  { get; set; }
-        public int                     CreationDate { get; set; }
-        public int                     ExpireDate   { get; set; }
-        public long                    UserId       { get; set; }
-        public User                    User         { get; set; }
-        public byte                    ExtensionId  { get; set; }
-        public SupportedMediaExtension Extension    { get; set; }
+        public bool                     Verified     { get; set; }
+        public byte                     MaxSizeInMb  { get; set; }
+        public int                      CreationDate { get; set; }
+        public int                      ExpireDate   { get; set; }
+        public long                     UserId       { get; set; }
+        public User                     User         { get; set; }
+        public byte?                    ExtensionId  { get; set; }
+        public SupportedMediaExtension? Extension    { get; set; }
     }
 }
