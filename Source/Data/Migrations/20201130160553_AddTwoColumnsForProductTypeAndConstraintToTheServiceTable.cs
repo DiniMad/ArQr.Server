fea485deb127bd4752855981657ab4d6ebfd58d5
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-30 19:36:56 +0330

    Add migration files for adding two columns to the service table.

---
+++ Source/Data/Migrations/20201130160553_AddTwoColumnsForProductTypeAndConstraintToTheServiceTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class AddTwoColumnsForProductTypeAndConstraintToTheServiceTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Constraint",
+                table: "Services",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "ProductType",
+                table: "Services",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Constraint",
+                table: "Services");
+
+            migrationBuilder.DropColumn(
+                name: "ProductType",
+                table: "Services");
+        }
+    }
+}
