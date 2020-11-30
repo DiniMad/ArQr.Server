567dbed360c09c5b7be8f9ebdfaefaec8c95e80f
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-30 19:41:33 +0330

    Add two properties for product type and constraint into the service domain.

--- Source/Domain/Service.cs
+++ Source/Domain/Service.cs
@@ -4,9 +4,11 @@ namespace Domain
 {
     public class Service : BaseDomain<byte>
     {
-        public string Title                    { get; set; }
-        public string Description              { get; set; }
-        public int    UnitPriceInThousandToman { get; set; }
-        public bool   Active                   { get; set; }
+        public string      Title                    { get; set; }
+        public string      Description              { get; set; }
+        public int         UnitPriceInThousandToman { get; set; }
+        public ProductType ProductType              { get; set; }
+        public int         Constraint               { get; set; }
+        public bool        Active                   { get; set; }
     }
 }
