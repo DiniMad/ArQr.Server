f7061eb9eb767b702f24056723ec8524f9c49d1c
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-30 19:56:06 +0330

    Add the two "ProductType" and "Constraint" properties to the create service resource.

--- Source/Resource/Api/Resources/ServiceResources.cs
+++ Source/Resource/Api/Resources/ServiceResources.cs
@@ -1,5 +1,10 @@
 namespace Resource.Api.Resources
 {
     public sealed record ServiceResource(byte Id, string Title, string Description, int UnitPriceInThousandToman);
-    public sealed record CreateServiceResource(string Title, string Description, int UnitPriceInThousandToman);
+
+    public sealed record CreateServiceResource(string Title,
+                                               string Description,
+                                               int    UnitPriceInThousandToman,
+                                               string ProductType,
+                                               int    Constraint);
 }
