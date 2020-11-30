26f7be475b1428022c764432622ad0da4988fb68
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-30 22:20:12 +0330

    Add migration files for making the extension id column in the media content table nullabel.

---
+++ Source/Data/Migrations/20201130184614_MakeTheExtensionIdColumnOfMediaContentNullable.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class MakeTheExtensionIdColumnOfMediaContentNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MediaContents_SupportedMediaExtensions_ExtensionId",
+                table: "MediaContents");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "ExtensionId",
+                table: "MediaContents",
+                type: "tinyint",
+                nullable: true,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MediaContents_SupportedMediaExtensions_ExtensionId",
+                table: "MediaContents",
+                column: "ExtensionId",
+                principalTable: "SupportedMediaExtensions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MediaContents_SupportedMediaExtensions_ExtensionId",
+                table: "MediaContents");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "ExtensionId",
+                table: "MediaContents",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0,
+                oldClrType: typeof(byte),
+                oldType: "tinyint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MediaContents_SupportedMediaExtensions_ExtensionId",
+                table: "MediaContents",
+                column: "ExtensionId",
+                principalTable: "SupportedMediaExtensions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
