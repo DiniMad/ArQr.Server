e8567c498eeb5af6976eb368d3814e0e1048026e
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-30 19:21:58 +0330

    Create a validation file for the create service resource.

---
+++ Source/Resource/Api/Validations/CreateServiceValidation.cs
@@ -0,0 +1,28 @@
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+using Resource.Api.Resources;
+using Resource.ResourceFiles;
+
+namespace Resource.Api.Validations
+{
+    public class CreateServiceValidation : AbstractValidator<CreateServiceResource>
+    {
+        public CreateServiceValidation(IStringLocalizer<ResourcesPropertyNames> propertyNames)
+        {
+            RuleFor(resource => resource.Title)
+                .NotNull()
+                .NotEmpty()
+                .WithName(_ => propertyNames[ResourcesPropertyNames.Title]);
+
+            RuleFor(resource => resource.Description)
+                .NotNull()
+                .NotEmpty()
+                .WithName(_ => propertyNames[ResourcesPropertyNames.Description]);
+
+            RuleFor(resource => resource.UnitPriceInThousandToman)
+                .NotNull()
+                .GreaterThan(0)
+                .WithName(_ => propertyNames[ResourcesPropertyNames.UnitPriceInThousandToman]);
+        }
+    }
+}
