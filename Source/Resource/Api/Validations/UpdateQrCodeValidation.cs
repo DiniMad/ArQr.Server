42694f7f7d082d820cc3af1cb8daa3add4be18de
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-30 21:27:18 +0330

    Create a validation file for the update qr code resource.

---
+++ Source/Resource/Api/Validations/UpdateQrCodeValidation.cs
@@ -0,0 +1,29 @@
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+using Resource.Api.Resources;
+using Resource.ResourceFiles;
+
+namespace Resource.Api.Validations
+{
+    public class UpdateQrCodeValidation : AbstractValidator<UpdateQrCodeResource>
+    {
+        public UpdateQrCodeValidation(IStringLocalizer<ResourcesPropertyNames> propertyNames)
+        {
+            RuleFor(resource => resource.Title)
+                .MaximumLength(32)
+                .WithName(_ => propertyNames[ResourcesPropertyNames.Title]);
+
+            RuleFor(resource => resource.Description)
+                .MaximumLength(128)
+                .WithName(_ => propertyNames[ResourcesPropertyNames.Description]);
+
+            RuleFor(resource => resource.AssociatedWebsite)
+                .MaximumLength(64)
+                .WithName(_ => propertyNames[ResourcesPropertyNames.AssociatedWebsite]);
+
+            RuleFor(resource => resource.AssociatedPhoneNumber)
+                .Length(10)
+                .WithName(_ => propertyNames[ResourcesPropertyNames.AssociatedPhoneNumber]);
+        }
+    }
+}
