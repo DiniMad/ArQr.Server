0d3406c4244a5fe895d0bcb0ee243fe3e1c06cac
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-11-30 19:15:20 +0330

    Create a resource record for creating service.

--- Source/Resource/Api/Resources/ServiceResources.cs
+++ Source/Resource/Api/Resources/ServiceResources.cs
@@ -1,4 +1,5 @@
 namespace Resource.Api.Resources
 {
     public sealed record ServiceResource(byte Id, string Title, string Description, int UnitPriceInThousandToman);
+    public sealed record CreateServiceResource(string Title, string Description, int UnitPriceInThousandToman);
 }
