cc4526dbc0b05883aa8f6403b40d26b045106f54
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-12-01 20:59:22 +0330

    Create a resource record for the upload session.

--- Source/Resource/Api/Resources/FileResources.cs
+++ Source/Resource/Api/Resources/FileResources.cs
@@ -7,4 +7,6 @@ namespace Resource.Api.Resources
     public sealed record UploadChunkResource(long MediaContentId, byte ChunkNumber, byte[] Content);
 
     public sealed record UploadCompletedResource(long MediaContentId);
+
+    public sealed record UploadSessionResource(int ChunkSize);
 }
