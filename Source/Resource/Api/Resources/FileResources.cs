c5d2994441d78e5b4dc0c383a6a836a2c4ba5499
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-12-01 19:06:08 +0330

    Remove the redundant upload session resource.

--- Source/Resource/Api/Resources/FileResources.cs
+++ Source/Resource/Api/Resources/FileResources.cs
@@ -4,8 +4,6 @@ namespace Resource.Api.Resources
 {
     public sealed record CreateUploadSessionResource(long MediaContentId, string Extension, byte TotalSizeInMb);
 
-    public sealed record UploadSessionResource(long MediaContentId);
-
     public sealed record UploadChunkResource(long MediaContentId, byte ChunkNumber, byte[] Content);
 
     public sealed record UploadCompletedResource(Guid Session);
