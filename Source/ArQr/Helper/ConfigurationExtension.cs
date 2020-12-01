9a0845da97f4cc4a971b5235d1b3f41058045728
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-12-01 20:57:28 +0330

    Add an extension method for the configuration type to get the file chunks options from appsettings file.

--- Source/ArQr/Helper/ConfigurationExtension.cs
+++ Source/ArQr/Helper/ConfigurationExtension.cs
@@ -14,10 +14,17 @@ namespace ArQr.Helper
         {
             return configuration.GetSection("CacheOptions").Get<CacheOptions>();
         }
-        
+
         public static string GetFileStoragePath(this IConfiguration configuration)
         {
             return configuration.GetValue<string>("FileStoragePath");
+        }
+
+        public static FileChunksOptions GetFileChunksOptions(this IConfiguration configuration)
+        {
+            return configuration.GetSection("FileChunksOptions")
+                                .Get<FileChunksOptions>(options =>
+                                                            options.BindNonPublicProperties = true);
         }
     }
 }
