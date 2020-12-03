e1ca9ddbaab12258be7f188a67df4ec3ba161bab
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-12-04 01:45:08 +0330

    Add the media content id to the "UpdateQrCodeResource".

--- Source/Resource/Api/Resources/QrCodeResources.cs
+++ Source/Resource/Api/Resources/QrCodeResources.cs
@@ -24,8 +24,9 @@ namespace Resource.Api.Resources
 
     public sealed record QrCodeViewersCountResource(int Count);
 
-    public sealed record UpdateQrCodeResource(string  Title,
-                                              string  Description,
+    public sealed record UpdateQrCodeResource(string? Title,
+                                              string? Description,
                                               string? AssociatedPhoneNumber,
-                                              string? AssociatedWebsite);
+                                              string? AssociatedWebsite,
+                                              long?   MediaContentId);
 }
