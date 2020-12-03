646d61217445172022757f806832d717f7c85037
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-12-03 06:48:42 +0330

    Make the database migration class disposeable.

--- Source/Data/DatabaseMigrator.cs
+++ Source/Data/DatabaseMigrator.cs
@@ -1,16 +1,24 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace Data
 {
-    public class DatabaseMigrator
+    public class DatabaseMigrator : IAsyncDisposable
     {
+        private readonly ApplicationDbContext _dbContext;
+
         public DatabaseMigrator()
         {
             var dbContextFactory = new DesignTimeDbContextFactory();
-            var dbContext        = dbContextFactory.CreateDbContext(Array.Empty<string>());
-            if (dbContext.Database.GetPendingMigrations().Any()) dbContext.Database.Migrate();
+            _dbContext = dbContextFactory.CreateDbContext(Array.Empty<string>());
+            if (_dbContext.Database.GetPendingMigrations().Any()) _dbContext.Database.Migrate();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _dbContext.DisposeAsync();
         }
     }
 }
