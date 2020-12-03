473a910af84dfe0332a13f3f0ad2f474c2cb09bf
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-12-03 08:44:10 +0330

    Add migration files for the navigation property from the qr code table to the media content table.

---
+++ Source/Data/Migrations/20201203051215_CreateOptionalOneToOneRelationFromQrCodeToMediaContent.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class CreateOptionalOneToOneRelationFromQrCodeToMediaContent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "MediaContentId",
+                table: "QrCodes",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QrCodes_MediaContentId",
+                table: "QrCodes",
+                column: "MediaContentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QrCodes_MediaContents_MediaContentId",
+                table: "QrCodes",
+                column: "MediaContentId",
+                principalTable: "MediaContents",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_QrCodes_MediaContents_MediaContentId",
+                table: "QrCodes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_QrCodes_MediaContentId",
+                table: "QrCodes");
+
+            migrationBuilder.DropColumn(
+                name: "MediaContentId",
+                table: "QrCodes");
+        }
+    }
+}
