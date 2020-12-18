1a78687fbe966608133f710621f820fbafe0a118
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-12-18 17:11:04 +0330

    Register the "AntDesign" service.

--- Source/Front/Blazor/Program.cs
+++ Source/Front/Blazor/Program.cs
@@ -1,12 +1,8 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace Blazor
 {
@@ -19,6 +15,8 @@ namespace Blazor
 
             builder.Services.AddScoped(sp => new HttpClient
                                            {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+
+            builder.Services.AddAntDesign();
 
             await builder.Build().RunAsync();
         }
