4dceb2b75d5215e989074d5f76f5da353bca1c08
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-12-19 13:30:56 +0330

    Extract service registry to a method.

--- Source/Front/Blazor/Program.cs
+++ Source/Front/Blazor/Program.cs
@@ -1,24 +1,33 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Blazor.Helpers;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Blazor
 {
     public class Program
     {
+        private static IConfiguration Configuration { get; set; }
+
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+            Configuration = builder.Configuration;
 
-            builder.Services.AddScoped(sp => new HttpClient
-                                           {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
-
-            builder.Services.AddAntDesign();
+            RegisterServices(builder.Services);
 
             await builder.Build().RunAsync();
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped(_ => new HttpClient {BaseAddress = new Uri(Configuration.Endpoints().Root)});
+            services.AddTransient(s => s.GetRequiredService<IConfiguration>().Endpoints());
+            services.AddAntDesign();
         }
     }
 }
