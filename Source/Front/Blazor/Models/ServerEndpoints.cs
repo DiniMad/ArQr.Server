775c8fda56b1b2c3fadb91f22de71a3e868b8e8b
Seyed Hossein Hosseini <payoff900@gmail.com>
2020-12-19 13:56:08 +0330

    Add the register endpoint to the appsettings and the ServerEndpoints model.

--- Source/Front/Blazor/Models/ServerEndpoints.cs
+++ Source/Front/Blazor/Models/ServerEndpoints.cs
@@ -2,7 +2,8 @@ namespace Blazor.Models
 {
     public sealed record ServerEndpoints
     {
-        public string Root  { get; set; }
-        public string Login { get; set; }
+        public string Root     { get; set; }
+        public string Login    { get; set; }
+        public string Register { get; set; }
     }
 }
