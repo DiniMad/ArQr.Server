eca5db8de1fecc3728e7228c286df2636b17d7ef
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-01-22 17:40:51 +0330

    Modify the Qrcode component to render a card with qr code on it.

--- Source/Front/Blazor/Pages/DashboardPage/Qrcode.razor.cs
+++ Source/Front/Blazor/Pages/DashboardPage/Qrcode.razor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Blazor.Helpers;
 using Microsoft.AspNetCore.Components;
@@ -7,13 +8,14 @@ namespace Blazor.Pages.DashboardPage
     public partial class Qrcode
     {
         [Parameter] public  long         Id          { get; set; }
+        [Parameter] public  string       Title       { get; set; }
         [Inject]    private JsFunctions  JsFunctions { get; set; }
-        private             MarkupString _qrcode;
+        private static      MarkupString _qrcodeMarkup;
 
         protected override async Task OnParametersSetAsync()
         {
             var qrcodeSvg = await JsFunctions.QrcodeSvgAsync(Id.ToString());
-            _qrcode = new MarkupString(qrcodeSvg);
+            _qrcodeMarkup = new MarkupString(qrcodeSvg);
         }
     }
 }
